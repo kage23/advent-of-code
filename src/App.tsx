f9b77da9685ed8f9bc25ff4f44b3d0122d5accb5
Kyle Sessions <ksessions@quantcast.com>
2018-12-20 14:33:33 -0800

    Convert to TypeScript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+class App extends Component<{}, {}> {
   render() {
     return (
       <div
@@ -15,4 +15,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
