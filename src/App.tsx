dac8f7a790e8d72e0dc0634184e754c52e3ef28d
Kyle Sessions <kage23@gmail.com>
2018-12-25 22:45:47 -0800

    Note the other things I want to do, maybe.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,12 @@ class App extends Component<{}, {}> {
         }}
       >
         <p>These are some of the things I wrote for the 2018 Advent of Code project. I completed the whole thing (hopefully, at least - I'm writing this on the 19th!) but I didn't start saving them to Github until I was a ways into them! Anyway, check out the different branches for the different days.</p>
+        <p>Bonus Items:</p>
+        <ul>
+          <li>Re-create Days 1-14 and put them on GitHub. (I'm pretty sure at least one of them is still on my laptop in the checkers-fe project.)</li>
+          <li>Render the map for Day 20.</li>
+          <li>Render the map for Day 22.</li>
+        </ul>
       </div>
     );
   }
