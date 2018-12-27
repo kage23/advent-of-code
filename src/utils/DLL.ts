355341c9a634cb8992201bf71ddc46c84aefeada
Kyle Sessions <ksessions@quantcast.com>
2018-12-26 16:10:20 -0800

    Add a Doubly-Linked Lists class.

---
+++ src/utils/DLL.ts
@@ -0,0 +1,55 @@
+class DLL {
+  head: INode | undefined
+
+  constructor(value?: any) {
+    if (value) {
+      const node: INode = { value }
+      node.next = node
+      node.prev = node
+      this.head = node
+    }
+  }
+
+  push = (value: any) => {
+    const node: INode = { value }
+    if (this.head) {
+      node.next = this.head
+      node.prev = this.head.prev
+      if (this.head.prev) this.head.prev.next = node
+      this.head.prev = node
+    } else {
+      node.next = node
+      node.prev = node
+      this.head = node
+    }
+  }
+
+  removeNode = (node: INode) => {
+    if (this.head === node) {
+      if (this.head.next === node) {
+        this.head = undefined
+      } else {
+        if (this.head.next) this.head.next.prev = this.head.prev
+        if (this.head.prev) this.head.prev.next = this.head.next
+        this.head = this.head.next
+      }
+    } else {
+      let currentNode = this.head
+      while (currentNode !== node) currentNode = currentNode ? currentNode.next : undefined
+      const currentPrev = currentNode.prev
+      const currentNext = currentNode.next
+      if (currentPrev && currentNext) {
+        currentNext.prev = currentPrev
+        currentPrev.next = currentNext
+      }
+    }
+  }
+}
+
+export interface INode {
+  value: any
+  next?: INode
+  prev?: INode
+}
+
+export default DLL
