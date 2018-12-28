04aa113248b3780d4a9b6b750afb3f2703fdb9b0
Kyle Sessions <ksessions@quantcast.com>
2018-12-28 15:13:51 -0800

    Add input selectors.
    I should have done this ages ago. :P

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { INPUT } from './Input'
 
-class App extends Component<{}, {}> {
+class App extends Component<{}, {
+  input: string
+}> {
+  constructor(props: {}) {
+    super(props)
+
+    this.state = {
+      input: ''
+    }
+  }
+
+  handleInputChange = (key: string) => {
+    this.setState({
+      input: key
+    })
+  }
+
   render() {
+    const { input } = this.state
+
+    const inputSelectors = []
+
+    for (const key of Object.keys(INPUT)) {
+      inputSelectors.push(([
+        <label key={key}>
+          <input
+            type="radio"
+            name="inputType"
+            value={key}
+            checked={input === key}
+            onChange={() => this.handleInputChange(key) }
+          />
+          {key}
+        </label>,
+        ' '
+      ]))
+    }
+
     return (
       <div
         style={{
@@ -10,6 +47,9 @@ class App extends Component<{}, {}> {
         }}
       >
         <p>These are some of the things I wrote for the 2018 Advent of Code project. I completed the whole thing (hopefully, at least - I'm writing this on the 19th!) but I didn't start saving them to Github until I was a ways into them! Anyway, check out the different branches for the different days.</p>
+        {inputSelectors.length > 0 && (
+          <fieldset>{inputSelectors}</fieldset>
+        )}
         <p>Bonus Items:</p>
         <ul>
           <li>Re-create Days 1-14 and put them on GitHub. (I'm pretty sure at least one of them is still on my laptop in the checkers-fe project.)</li>
--- src/Input.tsx
+++ src/Input.tsx
@@ -1,1 +1,2 @@
-export const INPUT = ``
+export const INPUT: { [key:string]: string } = {
+}
