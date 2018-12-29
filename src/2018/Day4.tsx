707a0d4f4f81fdca0023a5b7d6806bb77566bc24
Kyle Sessions <kage23@gmail.com>
2018-12-28 22:04:14 -0800

    Part 2 - Overall sleepiest guard-minute.

--- src/2018/Day4.tsx
+++ src/2018/Day4.tsx
@@ -10,6 +10,15 @@ import INPUT from './Input/Day4'
 let answer1_a = ''
 let answer1_b = ''
 
+let answer2_a = ''
+let answer2_b = ''
+
+const pathKey = (id: number, minute: number): string => `${id}_${minute}`
+const unPathKey = (input: string): { id: number, minute: number } => {
+  const [ id, minute ] = input.split('_')
+  return { id: parseInt(id), minute: parseInt(minute) }
+}
+
 const parseInput = (input: string) => {
   return input.split('\n')
   .sort((a, b) => a < b ? -1 : 1)
@@ -65,10 +74,50 @@ const part1 = (inputStr: string) => {
   }
 }
 
+const part2 = (inputStr: string) => {
+  const records = parseInput(INPUT[inputStr])
+  const guardSleepMinuteCount: { [key:string]: number } = {}
+
+  let currentGuardId = 0
+  let sleepMinute = 0
+  let wakeMinute = 0
+  for (const record of records) {
+    if (record.indexOf('begins shift') !== -1) currentGuardId = parseInt(record.slice(record.indexOf('#') + 1))
+    else if (record.indexOf('falls asleep') !== -1) sleepMinute = parseInt(record.slice(record.indexOf(':') + 1))
+    else if (record.indexOf('wakes up') !== -1) {
+      wakeMinute = parseInt(record.slice(record.indexOf(':') + 1))
+      for (let x = sleepMinute; x < wakeMinute; x++) {
+        if (typeof guardSleepMinuteCount[pathKey(currentGuardId, x)] === 'undefined') guardSleepMinuteCount[pathKey(currentGuardId, x)] = 0
+        guardSleepMinuteCount[pathKey(currentGuardId, x)]++
+      }
+    }
+  }
+
+  let highestGuardMinMinutes = 0
+  let bestGuardMin = { id: 0, minute: 0 }
+  for (const guardMin in guardSleepMinuteCount) {
+    if (guardSleepMinuteCount[guardMin] > highestGuardMinMinutes) {
+      highestGuardMinMinutes = guardSleepMinuteCount[guardMin]
+      bestGuardMin = unPathKey(guardMin)
+    }
+  }
+
+  answer2_a = bestGuardMin.id.toString()
+  answer2_b = bestGuardMin.minute.toString()
+
+  return {
+    answer2: (bestGuardMin.id * bestGuardMin.minute).toString()
+  }
+}
+
 const BUTTONS: IButton[] = [
   {
     label: 'Strategy 1',
     onClick: part1
+  },
+  {
+    label: 'Strategy 2',
+    onClick: part2
   }
 ]
 
@@ -82,8 +131,8 @@ const config: IDayConfig = {
   ),
   answer2Text: (answer) => (
     <span>
-      The ID of the only non-overlapping claim is ID #
-      <code>{answer}</code>.
+      The sleepiest guard-minute is <code>#{answer2_a}-{answer2_b}</code>.
+      The solution is <code>{answer}</code>.
     </span>
   ),
   buttons: BUTTONS,
