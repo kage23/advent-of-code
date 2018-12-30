4b3ee608e02e9016379284a3993376945a833a4c
Kyle Sessions <kage23@gmail.com>
2018-12-29 18:32:06 -0800

    Calculate root node value. (Part 2)

--- src/2018/Day8.tsx
+++ src/2018/Day8.tsx
@@ -25,22 +25,59 @@ const calculateChecksum = (numbers: number[]): { answer1: string } => {
   }
 }
 
+const calculateRootNodeValue = (inputKey: string): { answer2: string } => {
+  const numbers: number[] = INPUT[inputKey].split(' ').map(x => parseInt(x))
+  return {
+    answer2: calculateNodeValue(numbers).toString()
+  }
+}
+
+const calculateNodeValue = (numbers: number[]): number => {
+  let total = 0
+  let childNodeCount = numbers.shift() || 0
+  let childNodeValues: number[] = []
+  let metadataNodeCount = numbers.shift() || 0
+  if (childNodeCount === 0) {
+    while (metadataNodeCount > 0) {
+      total += numbers.shift() || 0
+      metadataNodeCount--
+    }
+    return total
+  } else {
+    while (childNodeCount > 0) {
+      childNodeValues.push(calculateNodeValue(numbers))
+      childNodeCount--
+    }
+    while (metadataNodeCount > 0) {
+      const childID = numbers.shift()
+      total += typeof childID === 'number' ? childNodeValues[childID - 1] || 0 : 0
+      metadataNodeCount--
+    }
+  }
+
+  return total
+}
+
 const BUTTONS: IButton[] = [
   {
     label: 'Calculate Checksum',
     onClick: (inputKey: string) => calculateChecksum(INPUT[inputKey].split(' ').map(x => parseInt(x)))
+  },
+  {
+    label: 'Calculate Root Node Value',
+    onClick: (inputKey: string) => calculateRootNodeValue(inputKey)
   }
 ]
 
 const config: IDayConfig = {
   answer1Text: (answer) => (
     <span>
-      The solution is <code>{answer}</code>.
+      The checksum is <code>{answer}</code>.
     </span>
   ),
   answer2Text: (answer) => (
     <span>
-      The checksum is <code>{answer}</code>.
+      The root node value is <code>{answer}</code>.
     </span>
   ),
   buttons: BUTTONS,
