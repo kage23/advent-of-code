2d127a3d73643c64751b28ccb6f880483d334b9a
Kyle Sessions <ksessions@quantcast.com>
2019-01-12 21:06:38 -0800

    Let's actually render the map.

--- src/2018/Day20.tsx
+++ src/2018/Day20.tsx
@@ -13,6 +13,7 @@ interface ICoord {
 
 interface IMapSquare extends ICoord {
   path?: string
+  doors?: string
 }
 
 interface IMap {
@@ -20,6 +21,13 @@ interface IMap {
   max: ICoord
   [key:string]: IMapSquare
 }
+
+let map: IMap = {
+  min: { x: 0, y: 0 },
+  max: { x: 0, y: 0 },
+  '0,0': { x: 0, y: 0 }
+}
+let prevInputKey = ''
 
 const pathKey = (coord: ICoord): string => `${coord.x},${coord.y}`
 
@@ -46,21 +54,25 @@ const createMap = (input: string, inMap?: IMap)
 
       case 'N':
         currentPath += char
+        addDoor(position, char, map)
         position = { x: position.x, y: position.y - 1 }
         break
 
       case 'E':
         currentPath += char
+        addDoor(position, char, map)
         position = { x: position.x + 1, y: position.y }
         break
 
       case 'W':
         currentPath += char
+        addDoor(position, char, map)
         position = { x: position.x - 1, y: position.y }
         break
 
         case 'S':
         currentPath += char
+        addDoor(position, char, map)
         position = { x: position.x, y: position.y + 1 }
         break
 
@@ -105,24 +117,28 @@ const calulateSplit = (input: string, map: IMap, inPosition: ICoord, inPath: str
     }
     switch (char) {
       case 'N':
+        addDoor(position, char, map)
         position = { x: position.x, y: position.y - 1 }
         paths[pathIndex] += char
         updateMap(position, paths[pathIndex], map)
         break
 
       case 'E':
+        addDoor(position, char, map)
         position = { x: position.x + 1, y: position.y }
         paths[pathIndex] += char
         updateMap(position, paths[pathIndex], map)
         break
 
       case 'W':
+        addDoor(position, char, map)
         position = { x: position.x - 1, y: position.y }
         paths[pathIndex] += char
         updateMap(position, paths[pathIndex], map)
         break
 
       case 'S':
+        addDoor(position, char, map)
         position = { x: position.x, y: position.y + 1 }
         paths[pathIndex] += char
         updateMap(position, paths[pathIndex], map)
@@ -152,17 +168,47 @@ const calulateSplit = (input: string, map: IMap, inPosition: ICoord, inPath: str
   }
 }
 
+const addDoor = (position: ICoord, door: string, map: IMap): IMap => {
+  const mapSquare = map[pathKey(position)]
+  if (mapSquare.doors !== undefined && mapSquare.doors.indexOf(door) === -1) mapSquare.doors += door
+  const otherRoomPosition = door === 'N'
+    ? { x: position.x, y: position.y - 1 }
+    : door === 'W'
+      ? { x: position.x - 1, y: position.y }
+      : door === 'E'
+        ? { x: position.x + 1, y: position.y }
+        : { x: position.x, y: position.y + 1 }
+  if (!map[pathKey(otherRoomPosition)]) {
+    map[pathKey(otherRoomPosition)] = {
+      x: otherRoomPosition.x,
+      y: otherRoomPosition.y,
+      doors: ''
+    }
+  }
+  const otherMapSquare = map[pathKey(otherRoomPosition)]
+  otherMapSquare.doors += door === 'W' ? 'E'
+    : door === 'E' ? 'W'
+    : door === 'N' ? 'S'
+    : door === 'S' ? 'N'
+    : ''
+  return map
+}
+
 const updateMap = (position: ICoord, path: string, map: IMap): IMap => {
-  if (!map[pathKey(position)]) {
+  let mapSquare = map[pathKey(position)]
+  if (!mapSquare) {
     map[pathKey(position)] = {
       ...position,
+      doors: '',
       path
     }
-    if (position.x < map.min.x) map.min.x = position.x
-    if (position.y < map.min.y) map.min.y = position.y
-    if (position.x > map.max.x) map.max.x = position.x
-    if (position.y > map.max.y) map.max.y = position.y
+    mapSquare = map[pathKey(position)]
   }
+  if (mapSquare.path === undefined) mapSquare.path = path
+  if (position.x < map.min.x) map.min.x = position.x
+  if (position.y < map.min.y) map.min.y = position.y
+  if (position.x > map.max.x) map.max.x = position.x
+  if (position.y > map.max.y) map.max.y = position.y
 
   return map
 }
@@ -170,11 +216,8 @@ const updateMap = (position: ICoord, path: string, map: IMap): IMap => {
 const BUTTONS: IButton[] = [
   {
     label: 'Find Farthest Room',
-    onClick: (inputKey: string) => {
-      const input = INPUT[inputKey]
-
+    onClick: () => {
       let farthestDistance = 0
-      const { map } = createMap(input)
       const { min, max } = map
 
       for (let xi = min.x; xi <= max.x; xi++)
@@ -190,11 +233,8 @@ const BUTTONS: IButton[] = [
   },
   {
     label: 'Count Far Rooms',
-    onClick: (inputKey: string) => {
-      const input = INPUT[inputKey]
-
+    onClick: () => {
       let farRooms = 0
-      const { map } = createMap(input)
       const { min, max } = map
 
       for (let xi = min.x; xi <= max.x; xi++)
@@ -210,18 +250,62 @@ const BUTTONS: IButton[] = [
   }
 ]
 
-const renderDay = (dayConfig: IDayConfig, inputKey: string): JSX.Element => (
-  <div className="render-box">
-    <div
-      style={{
-        maxWidth: '25%'
-      }}
-    >
-      <h3>Input:</h3>
-      <pre className="render-box--pre-wrap">{dayConfig.INPUT[inputKey]}</pre>
+const renderDay = (dayConfig: IDayConfig, inputKey: string): JSX.Element => {
+  if (prevInputKey !== inputKey) {
+    map = createMap(dayConfig.INPUT[inputKey]).map
+    prevInputKey = inputKey
+  }
+
+  const {
+    min,
+    max
+  } = map
+  const mapDisplay: JSX.Element[] = []
+  for (let yi = min.y; yi <= max.y; yi++) {
+    let aboveRow = ''
+    let currentRow = ''
+    for (let xi = min.x; xi <= max.x; xi++) {
+      const mapSquare = map[pathKey({ x: xi, y: yi })]
+      aboveRow += '#'
+      aboveRow += mapSquare.doors && mapSquare.doors.indexOf('N') !== -1
+        ? '-'
+        : '#'
+      currentRow += mapSquare.doors && mapSquare.doors.indexOf('W') !== -1
+        ? '|'
+        : '#'
+      currentRow += (xi === 0 && yi === 0)
+        ? 'X'
+        : '.'
+    }
+    aboveRow += '#'
+    currentRow += '#'
+    mapDisplay.push((
+      <div key={`${yi}a`}>{aboveRow}</div>
+    ))
+    mapDisplay.push((
+      <div key={`${yi}b`}>{currentRow}</div>
+    ))
+  }
+  let currentRow = ''
+  for (let i = 0; i < ((max.x - min.x + 1) * 2) + 1; i++) currentRow += '#'
+  mapDisplay.push((
+    <div key={`final`}>{currentRow}</div>
+  ))
+
+  return (
+    <div className="render-box render-box--no-wrap">
+      <div style={{ maxWidth: '25%' }}>
+        <h3>Input:</h3>
+        <pre className="render-box--pre-wrap">{dayConfig.INPUT[inputKey]}</pre>
+      </div>
+      <div style={{ marginLeft: '24px' }}>
+        <fieldset>
+          {mapDisplay}
+        </fieldset>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 const config: IDayConfig = {
   answer1Text: (answer) => (
