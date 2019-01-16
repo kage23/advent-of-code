dac01aa7cb00c5e1cfe0ed253630f91d6f9bdb5e
Kyle Sessions <ksessions@quantcast.com>
2019-01-15 16:32:00 -0800

    Extract 2018 year config out into separate file.

---
+++ src/2018/Config.tsx
@@ -0,0 +1,60 @@
+import { IYearConfig } from '../Config'
+
+import Day1__2018__Config from './Day1'
+import Day2__2018__Config from './Day2'
+import Day3__2018__Config from './Day3'
+import Day4__2018__Config from './Day4'
+import Day5__2018__Config from './Day5'
+import Day6__2018__Config from './Day6'
+import Day7__2018__Config from './Day7'
+import Day8__2018__Config from './Day8'
+import Day9__2018__Config from './Day9'
+import Day10__2018__Config from './Day10'
+import Day11__2018__Config from './Day11'
+import Day12__2018__Config from './Day12'
+import Day13__2018__Config from './Day13'
+import Day14__2018__Config from './Day14'
+import Day15__2018__Config from './Day15'
+import Day16__2018__Config from './Day16'
+import Day17__2018__Config from './Day17'
+import Day18__2018__Config from './Day18'
+import Day19__2018__Config from './Day19'
+import Day20__2018__Config from './Day20'
+import Day21__2018__Config from './Day21'
+import Day22__2018__Config from './Day22'
+import Day23__2018__Config from './Day23'
+import Day24__2018__Config from './Day24'
+import Day25__2018__Config from './Day25'
+
+const CONFIG_2018: IYearConfig = {
+  year: 2018,
+  days: [
+    Day1__2018__Config,
+    Day2__2018__Config,
+    Day3__2018__Config,
+    Day4__2018__Config,
+    Day5__2018__Config,
+    Day6__2018__Config,
+    Day7__2018__Config,
+    Day8__2018__Config,
+    Day9__2018__Config,
+    Day10__2018__Config,
+    Day11__2018__Config,
+    Day12__2018__Config,
+    Day13__2018__Config,
+    Day14__2018__Config,
+    Day15__2018__Config,
+    Day16__2018__Config,
+    Day17__2018__Config,
+    Day18__2018__Config,
+    Day19__2018__Config,
+    Day20__2018__Config,
+    Day21__2018__Config,
+    Day22__2018__Config,
+    Day23__2018__Config,
+    Day24__2018__Config,
+    Day25__2018__Config
+  ]
+}
+
+export default CONFIG_2018
