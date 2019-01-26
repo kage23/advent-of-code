4ee6f50d2eadcf032afa12e1ed2416675b29ac76
Kyle Sessions <ksessions@quantcast.com>
2019-01-26 11:41:22 -0800

    Part 1.

--- src/2017/Day12.tsx
+++ src/2017/Day12.tsx
@@ -6,14 +6,79 @@ import {
 } from '../Config'
 
 import INPUT from './Input/Day12'
+import { number } from 'prop-types';
+
+interface IProgram {
+  connectedTo: number[]
+  inZeroGroup?: boolean
+}
+
+const parseInput = (input: string): Map<number, IProgram> => {
+  const programMap: Map<number, IProgram> = new Map()
+
+  input.split('\n').forEach(line => {
+    const [
+      originString,
+      connectedToString
+    ] = line.split(' <-> ')
+
+    programMap.set(parseInt(originString), {
+      connectedTo: connectedToString.split(', ').map(x => parseInt(x))
+    })
+  })
+
+  return programMap
+}
+
+const connectedToZero = (origin: number, program: IProgram, programMap: Map<number, IProgram>, parents: number[]): boolean => {
+  if (program.inZeroGroup) return true
+
+  const { connectedTo } = program
+
+  if (origin === 0) {
+    programMap.set(origin, {
+      connectedTo,
+      inZeroGroup: true
+    })
+    return true
+  } else {
+    const inZeroGroup = connectedTo.some(subOrigin => {
+      if (subOrigin === origin || parents.indexOf(subOrigin) !== -1) return false
+      const subProgram = programMap.get(subOrigin)
+      if (subProgram) {
+        if (subProgram.inZeroGroup) return true
+        const newParents = parents.concat(origin)
+        return connectedToZero(subOrigin, subProgram, programMap, newParents)
+      }
+      return false
+    })
+    program.inZeroGroup = inZeroGroup
+    return inZeroGroup
+  }
+}
 
 const BUTTONS: IButton[] = [
+  {
+    label: 'Find Programs Connected to 0',
+    onClick: (inputKey) => {
+      let programCount = 0
+      const programMap = parseInput(INPUT[inputKey])
+
+      for (const [id, program] of programMap) {
+        if (connectedToZero(id, program, programMap, [])) programCount++
+      }
+
+      return {
+        answer1: programCount.toString()
+      }
+    }
+  }
 ]
 
 const config: IDayConfig = {
   answer1Text: (answer) => (
     <span>
-      <code>{answer}</code>
+      <code>{answer}</code> programs are in the group that contains program ID <code>0</code>.
     </span>
   ),
   answer2Text: (answer) => (
