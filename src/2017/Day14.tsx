e235c9c3337b31dde4c14be5eda1cdd7770827ad
Kyle Sessions <ksessions@quantcast.com>
2019-01-28 18:56:37 -0800

    Part 2.

--- src/2017/Day14.tsx
+++ src/2017/Day14.tsx
@@ -19,15 +19,57 @@ const BUTTONS: IButton[] = [
         for (let j = 0; j < hash.length; j++) {
           grid += parseInt(hash[j], 16).toString(2).padStart(4, '0')
         }
-        grid += '\n'
       }
 
       return {
         answer1: grid.split('').filter(x => x === '1').length.toString()
       }
     }
+  },
+  {
+    label: 'Find Regions',
+    onClick: (inputKey) => {
+      const gridSize = 128
+      let regionCount = 0
+      const gridMap: Map<string, number> = new Map()
+      for (let i = 0; i < gridSize; i++) {
+        const hash = generateKnotHash(`${INPUT[inputKey]}-${i}`, 256)
+        for (let j = 0; j < hash.length; j++) {
+          const hex = parseInt(hash[j], 16).toString(2).padStart(4, '0')
+          gridMap.set(`${i},${(j * 4) + 0}`, parseInt(hex.charAt(0)))
+          gridMap.set(`${i},${(j * 4) + 1}`, parseInt(hex.charAt(1)))
+          gridMap.set(`${i},${(j * 4) + 2}`, parseInt(hex.charAt(2)))
+          gridMap.set(`${i},${(j * 4) + 3}`, parseInt(hex.charAt(3)))
+        }
+      }
+
+      for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+          const gridName = `${i},${j}`
+          const gridValue = gridMap.get(gridName)
+          if (gridValue === 1) {
+            regionCount++
+            removeGroup(i, j, gridMap)
+          }
+        }
+      }
+
+      return {
+        answer2: regionCount.toString()
+      }
+    }
   }
 ]
+
+const removeGroup = (x: number, y: number, gridMap: Map<string, number>): void => {
+  const gridValue = gridMap.get(`${x},${y}`)
+  if (!gridValue) return
+  gridMap.set(`${x},${y}`, 0)
+  removeGroup(x - 1, y, gridMap)
+  removeGroup(x + 1, y, gridMap)
+  removeGroup(x, y - 1, gridMap)
+  removeGroup(x, y + 1, gridMap)
+}
 
 const config: IDayConfig = {
   answer1Text: (answer) => (
@@ -37,7 +79,7 @@ const config: IDayConfig = {
   ),
   answer2Text: (answer) => (
     <span>
-      <code>{answer}</code>
+      There are <code>{answer}</code> regions.
     </span>
   ),
   buttons: BUTTONS,
