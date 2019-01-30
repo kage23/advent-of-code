c86cc51103f8cf7a37cefc8de93b779692bde532
Kyle Sessions <ksessions@quantcast.com>
2019-01-29 16:03:59 -0800

    Part 1.

--- src/2017/Day16.tsx
+++ src/2017/Day16.tsx
@@ -9,7 +9,74 @@ import INPUT from './Input/Day16'
 let prevInputKey = ''
 let programs = ''
 
+const exchange = (programs: string, a: number, b: number): string => {
+  const x = Math.min(a, b)
+  const y = Math.max(a, b)
+
+  return `${
+    programs.slice(0, x)
+  }${
+    programs.charAt(y)
+  }${
+    programs.slice(x + 1, y)
+  }${
+    programs.charAt(x)
+  }${
+    programs.slice(y + 1)
+  }`
+}
+
+const partner = (programs: string, a: string, b: string): string => {
+  const x = Math.min(programs.indexOf(a), programs.indexOf(b))
+  const y = Math.max(programs.indexOf(a), programs.indexOf(b))
+
+  return `${
+    programs.slice(0, x)
+  }${
+    programs.charAt(y)
+  }${
+    programs.slice(x + 1, y)
+  }${
+    programs.charAt(x)
+  }${
+    programs.slice(y + 1)
+  }`
+}
+
+const spin = (programs: string, x: number): string => (
+  `${programs.slice(x * -1)}${programs.slice(0, programs.length - x)}`
+)
+
+const dance = (programs: string, instruction: string): string => {
+  switch (instruction[0]) {
+    case 's':
+    return spin(programs, parseInt(instruction.slice(1)))
+
+    case 'x':
+    const [ a, b ] = instruction.slice(1).split('/')
+    return exchange(programs, parseInt(a), parseInt(b))
+
+    case 'p':
+    const [ c, d ] = instruction.slice(1).split('/')
+    return partner(programs, c, d)
+
+    default:
+    return ''
+  }
+}
+
 const BUTTONS: IButton[] = [
+  {
+    label: 'Dance',
+    onClick: (inputKey) => {
+      INPUT[inputKey].split(',').forEach(instruction => {
+        programs = dance(programs, instruction)
+      })
+      return {
+        answer1: programs
+      }
+    }
+  }
 ]
 
 const renderDay = (dayConfig: IDayConfig, inputKey: string): JSX.Element => {
