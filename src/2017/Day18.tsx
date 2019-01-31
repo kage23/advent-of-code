0b6a81bfcc42121d4b7fb3ed35e023597d002536
Kyle Sessions <kage23@gmail.com>
2019-01-30 17:53:45 -0800

    Part 2.

--- src/2017/Day18.tsx
+++ src/2017/Day18.tsx
@@ -13,6 +13,10 @@ interface IInstruction {
 }
 
 let prevInputKey = ''
+const rcvqs: number[][] = [
+  [],
+  []
+]
 
 const parseInput = (input: string): IInstruction[] => {
   return input.split('\n').map(line => {
@@ -34,39 +38,73 @@ const parseInput = (input: string): IInstruction[] => {
   })
 }
 
-const SOUND_PLAYER = {
-  currentInstruction: 0,
-  instructions: [],
-  lastPlayed: NaN,
-  registers: {},
-  snd: function (x) {
-    this.lastPlayed = this.registers[x]
-  },
-  set: function (x, y) {
+class Program {
+  currentInstruction: number
+  instructions: IInstruction[]
+  lastPlayed: number
+  registers: { [key:string]: number }
+  id?: number
+
+  constructor(part: 1 | 2, id?: number) {
+    this.currentInstruction = 0
+    this.instructions = []
+    this.lastPlayed = NaN
+    if (typeof id === 'number') this.id = id
+    if (part === 1) this.registers = {}
+    else {
+      this.registers = {
+        p: id || 0
+      }
+    }
+  }
+
+  snd = (inX: string, part: number) => {
+    const x = isNaN(parseInt(inX)) ? this.registers[inX] : parseInt(inX)
+    if (part === 1) this.lastPlayed = x
+    if (part === 2) {
+      const sendTo = ((this.id || 0) + 1) % 2
+      rcvqs[sendTo].push(x)
+    }
+  }
+
+  set = (x: string, y: string | number) => {
     this.registers[x] = typeof y === 'number'
       ? y
       : this.registers[y]
-  },
-  add: function (x, y) {
+  }
+
+  add = (x: string, y: string | number) => {
     this.registers[x] += typeof y === 'number'
-    ? y
-    : this.registers[y]
-  },
-  mul: function (x, y) {
+      ? y
+      : this.registers[y]
+  }
+
+  mul = (x: string, y: string | number) => {
     this.registers[x] *= typeof y === 'number'
-    ? y
-    : this.registers[y]
-  },
-  mod: function (x, y) {
+      ? y
+      : this.registers[y]
+  }
+
+  mod = (x: string, y: string | number) => {
     this.registers[x] %= typeof y === 'number'
-    ? y
-    : this.registers[y]
-  },
-  rcv: function (x) {
-    if (this.registers[x] !== 0) return this.lastPlayed
-  },
-  jgz: function (x, y) {
-    if (this.registers[x] > 0) {
+      ? y
+      : this.registers[y]
+  }
+
+  rcv = (x: string, part: number): number | void => {
+    if (part === 1 && this.registers[x] !== 0) return this.lastPlayed
+    if (part === 2) {
+      const rcvdValue = rcvqs[this.id || 0].shift()
+      if (typeof rcvdValue === 'number') {
+        this.registers[x] = rcvdValue
+      }
+      return rcvdValue
+    }
+  }
+
+  jgz = (inX: string, y: string | number) => {
+    const x = isNaN(parseInt(inX)) ? this.registers[inX] : parseInt(inX)
+    if (x > 0) {
       this.currentInstruction += (
         typeof y === 'number'
           ? y
@@ -74,32 +112,24 @@ const SOUND_PLAYER = {
       ) - 1 // The minus one is to allow us to skip ahead plus one like we do after every other instruction
     }
   }
-} as {
-  currentInstruction: number
-  instructions: IInstruction[]
-  lastPlayed: number
-  registers: { [key:string]: number }
-  snd: (x: string) => void
-  set: (x: string, y: string | number) => void
-  add: (x: string, y: string | number) => void
-  mul: (x: string, y: string | number) => void
-  mod: (x: string, y: string | number) => void
-  rcv: (x: string) => number | void
-  jgz: (x: string, y: string | number) => void
 }
 
-const executeInstruction = (command: string, x: string, inY: string | number | undefined): number | void => {
+let SOUND_PLAYER = new Program(1)
+
+const executeInstruction = (part: number, program: Program, command: string, x: string, inY: string | number | undefined): number | boolean | void => {
   if (!SOUND_PLAYER.registers[x]) SOUND_PLAYER.registers[x] = 0
   const y = typeof inY === 'undefined' ? '' : inY
   switch (command) {
-    case 'snd':
     case 'set':
     case 'add':
     case 'mul':
     case 'mod':
     case 'jgz':
+    return program[command](x, y)
+
     case 'rcv':
-    return SOUND_PLAYER[command](x, y)
+    case 'snd':
+    return program[command](x, part)
 
     default:
     break
@@ -108,26 +138,64 @@ const executeInstruction = (command: string, x: string, inY: string | number | u
 
 const BUTTONS: IButton[] = [
   {
-    label: 'Find Frequency',
+    label: 'Find Frequency (Part 1)',
     onClick: () => {
       let {
         command,
         x,
         y
       } = SOUND_PLAYER.instructions[SOUND_PLAYER.currentInstruction]
-      let result = executeInstruction(command, x, y)
+      let result = executeInstruction(1, SOUND_PLAYER, command, x, y)
       SOUND_PLAYER.currentInstruction++
 
       while (!result) {
         command = SOUND_PLAYER.instructions[SOUND_PLAYER.currentInstruction].command
         x = SOUND_PLAYER.instructions[SOUND_PLAYER.currentInstruction].x
         y = SOUND_PLAYER.instructions[SOUND_PLAYER.currentInstruction].y
-        result = executeInstruction(command, x, y)
+        result = executeInstruction(1, SOUND_PLAYER, command, x, y)
         SOUND_PLAYER.currentInstruction++
       }
 
       return {
         answer1: result.toString()
+      }
+    }
+  },
+  {
+    label: 'Run Both Programs (Part 2)',
+    onClick: (inputKey) => {
+      let program1Send = 0
+      const program0 = new Program(2, 0)
+      program0.instructions = parseInput(INPUT[inputKey])
+      const program1 = new Program(2, 1)
+      program1.instructions = parseInput(INPUT[inputKey])
+      let result0: number | boolean | void = false
+      let result1: number | boolean | void = false
+
+      while (!result0 || !result1) {
+        const instruction0 = program0.instructions[program0.currentInstruction]
+        const instruction1 = program1.instructions[program1.currentInstruction]
+        result0 = executeInstruction(2, program0, instruction0.command, instruction0.x, instruction0.y)
+        result1 = executeInstruction(2, program1, instruction1.command, instruction1.x, instruction1.y)
+        if (instruction1.command === 'snd') program1Send++
+        if (instruction0.command === 'rcv') {
+          if (typeof result0 === 'number') {
+            program0.currentInstruction++
+            result0 = undefined
+          } else result0 = true
+        } else program0.currentInstruction++
+        if (instruction1.command === 'rcv') {
+          if (typeof result1 === 'number') {
+            program1.currentInstruction++
+            result1 = undefined
+          } else result1 = true
+        } else program1.currentInstruction++
+        if (!program0.instructions[program0.currentInstruction]) result0 = true
+        if (!program1.instructions[program1.currentInstruction]) result1 = true
+      }
+
+      return {
+        answer2: program1Send.toString()
       }
     }
   }
@@ -136,6 +204,7 @@ const BUTTONS: IButton[] = [
 const renderDay = (dayConfig: IDayConfig, inputKey: string): JSX.Element => {
   if (prevInputKey !== inputKey) {
     prevInputKey = inputKey
+    SOUND_PLAYER = new Program(1)
     SOUND_PLAYER.instructions = parseInput(dayConfig.INPUT[inputKey])
   }
 
@@ -153,11 +222,11 @@ const renderDay = (dayConfig: IDayConfig, inputKey: string): JSX.Element => {
         <pre>{dayConfig.INPUT[inputKey]}</pre>
       </div>
       <div className="render-box--left-margin">
-        <h3>Registers:</h3>
+        <h3>Registers (Part 1):</h3>
         {registerDisplay}
       </div>
       <div className="render-box--left-margin">
-        <h3>Next Instruction:</h3>
+        <h3>Next Instruction (Part 1):</h3>
         <h3>
           {SOUND_PLAYER.currentInstruction}:{' '}
           {SOUND_PLAYER.instructions[SOUND_PLAYER.currentInstruction].command}{' '}
@@ -178,7 +247,8 @@ const config: IDayConfig = {
   ),
   answer2Text: (answer) => (
     <span>
-      <code>{answer}</code>
+      Program 1 sent a value{' '}
+      <code>{answer}</code> times.
     </span>
   ),
   buttons: BUTTONS,
--- src/2017/Input/Day18.tsx
+++ src/2017/Input/Day18.tsx
@@ -1,5 +1,5 @@
 const INPUT: { [key:string]: string } = {
-DEMO:
+DEMO_1:
 `set a 1
 add a 2
 mul a a
@@ -10,6 +10,15 @@ rcv a
 jgz a -1
 set a 1
 jgz a -2`,
+
+DEMO_2:
+`snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d`,
 
 REAL:
 `set i 31
