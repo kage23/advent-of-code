262bf397b52b38f1974cb6b505c154798b9aceff
Kyle Sessions <ksessions@quantcast.com>
2019-02-08 14:13:28 -0800

    Part 1.

--- src/2016/Day1.tsx
+++ src/2016/Day1.tsx
@@ -4,9 +4,44 @@ import {
   IDayConfig
 } from '../Config'
 
+import { manhattanDistance } from '../utils/Various'
+
 import INPUT from './Input/Day1'
 
+// increase directions index to turn right; decrease to turn left
+const DIRECTIONS = [
+  [-1, 0], // moving north
+  [0, 1], // moving east
+  [1, 0], // moving south
+  [0, -1] // moving west
+]
+
 const BUTTONS: IButton[] = [
+  {
+    label: 'Find Easter Bunny HQ',
+    onClick: inputKey => {
+      const directions = INPUT[inputKey].split(', ')
+      const currentPosition = [0, 0]
+      let currentDirectionIndex = 0
+
+      for (const directionRaw of directions) {
+        // Turn
+        currentDirectionIndex += directionRaw.charAt(0) === 'R'
+          ? 1 : -1
+        currentDirectionIndex = currentDirectionIndex % DIRECTIONS.length
+        if (currentDirectionIndex < 0) currentDirectionIndex = DIRECTIONS.length - 1
+
+        // Then move
+        const numberOfBlocks = parseInt(directionRaw.slice(1))
+        currentPosition[0] += (DIRECTIONS[currentDirectionIndex][0] * numberOfBlocks)
+        currentPosition[1] += (DIRECTIONS[currentDirectionIndex][1] * numberOfBlocks)
+      }
+
+      return {
+        answer1: manhattanDistance(currentPosition, [0, 0]).toString()
+      }
+    }
+  }
 ]
 
 const renderDay = (dayConfig: IDayConfig, inputKey: string): JSX.Element => (
