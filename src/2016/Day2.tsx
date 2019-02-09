2f21c3bcade08a5bece06d31385e8298ff9770d6
Kyle Sessions <ksessions@quantcast.com>
2019-02-08 16:19:22 -0800

    Part 2.

--- src/2016/Day2.tsx
+++ src/2016/Day2.tsx
@@ -7,16 +7,16 @@ import {
 
 import INPUT from './Input/Day2'
 
-const DOOR_BUTTONS = [
-  '123',
-  '456',
-  '789'
-]
-
 const BUTTONS: IButton[] = [
   {
-    label: 'Find Bathroom Code',
+    label: 'Find Bathroom Code (Phonepad Numbers)',
     onClick: inputKey => {
+      const DOOR_BUTTONS = [
+        '123',
+        '456',
+        '789'
+      ]
+
       const inputLines = INPUT[inputKey].split('\n')
       const currentPosition = [1, 1]
       let code = ''
@@ -51,6 +51,69 @@ const BUTTONS: IButton[] = [
         answer1: code
       }
     }
+  },
+  {
+    label: 'Find Bathroom Code (Real Buttons)',
+    onClick: inputKey => {
+      const DOOR_BUTTONS = [
+        '  1  ',
+        ' 234 ',
+        '56789',
+        ' ABC ',
+        '  D  '
+      ]
+      const buttonRowLimits = [
+        [2, 2],
+        [1, 3],
+        [0, 4],
+        [1, 3],
+        [2, 2]
+      ]
+
+      const inputLines = INPUT[inputKey].split('\n')
+      const currentPosition = [0, 2]
+      let code = ''
+
+      for (const line of inputLines) {
+        for (let i = 0; i < line.length; i++) {
+          switch (line.charAt(i)) {
+            case 'U':
+            if (
+              currentPosition[1] - 1 >= 0
+              && DOOR_BUTTONS[currentPosition[1] - 1].charAt(currentPosition[0]) !== ' '
+            ) {
+              currentPosition[1] = Math.max(0, currentPosition[1] - 1)
+            }
+            break
+
+            case 'D':
+            if (
+              currentPosition[1] + 1 < DOOR_BUTTONS.length
+              && DOOR_BUTTONS[currentPosition[1] + 1].charAt(currentPosition[0]) !== ' '
+            ) {
+              currentPosition[1] += 1
+            }
+            break
+
+            case 'L':
+            currentPosition[0] = Math.max(buttonRowLimits[currentPosition[1]][0], currentPosition[0] - 1)
+            break
+
+            case 'R':
+            currentPosition[0] = Math.min(buttonRowLimits[currentPosition[1]][1], currentPosition[0] + 1)
+            break
+
+            default:
+            break
+          }
+        }
+        code += DOOR_BUTTONS[currentPosition[1]].charAt(currentPosition[0])
+      }
+
+      return {
+        answer2: code
+      }
+    }
   }
 ]
 
@@ -63,7 +126,8 @@ const config: IDayConfig = {
   ),
   answer2Text: (answer) => (
     <span>
-      <code>{answer}</code>
+      The real bathroom code is{' '}
+      <code>{answer}</code>.
     </span>
   ),
   buttons: BUTTONS,
