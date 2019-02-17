b1a714c9adeb96106f752ee907d7b3a6c59996ab
Kyle Sessions <kage23@gmail.com>
2019-02-16 21:01:24 -0800

    Part 2.

--- src/2016/Day10.tsx
+++ src/2016/Day10.tsx
@@ -14,12 +14,12 @@ interface IBot {
   high?: { to: 'bot' | 'output', id: number }
 }
 
-const getBots = (inputKey: string): Map<number, IBot> => {
-  const bots: Map<number, IBot> = new Map()
+const getBots = (inputKey: string): IBot[] => {
+  const bots: IBot[] = []
 
   INPUT[inputKey].split('\n').forEach(line => {
     const botId = parseInt(line.split('bot ')[1])
-    const bot = bots.get(botId) || { id: botId }
+    const bot = bots.find(bot => bot.id === botId) || { id: botId }
     if (line.startsWith('value')) {
       const value = parseInt(line.split('value ')[1])
       if (!bot.numbers) bot.numbers = []
@@ -50,64 +50,90 @@ const getBots = (inputKey: string): Map<number, IBot> => {
         }
       }
     }
-    bots.set(botId, bot)
+    bots.push(bot)
   })
 
   return bots
+}
+
+const runBots = (inputKey: string): {
+  bots: IBot[]
+  outputs: Map<number, number>
+  answer1: undefined | string
+} => {
+  const bots = getBots(inputKey)
+  const outputs: Map<number, number> = new Map()
+  let answer1: undefined | string = undefined
+
+  const comparisonNumbers = inputKey.indexOf('DEMO') !== -1
+  ? [2, 5]
+  : [17, 61]
+
+  while (bots.some(bot => bot.numbers ? bot.numbers.length === 2 : false)) {
+    botLoop:
+    for (const bot of bots) {
+      if (!bot.numbers || bot.numbers.length !== 2) continue botLoop
+      const low = Math.min(...bot.numbers)
+      const high = Math.max(...bot.numbers)
+      bot.numbers = []
+      if (bot.low) {
+        const botLowId = bot.low.id
+        if (bot.low.to === 'bot') {
+          const toBotLow = bots.find(findBot => findBot.id === botLowId)
+          if (toBotLow) {
+            if (!toBotLow.numbers) toBotLow.numbers = []
+            toBotLow.numbers.push(low)
+          }
+        }
+        if (bot.low.to === 'output') {
+          outputs.set(bot.low.id, low)
+        }
+      }
+      if (bot.high) {
+        if (bot.high.to === 'bot') {
+          const botHighId = bot.high.id
+          const toBotHigh = bots.find(findBot => findBot.id === botHighId)
+          if (toBotHigh) {
+            if (!toBotHigh.numbers) toBotHigh.numbers = []
+            toBotHigh.numbers.push(high)
+          }
+        }
+        if (bot.high.to === 'output') {
+          outputs.set(bot.high.id, high)
+        }
+      }
+      if (low === comparisonNumbers[0] && high === comparisonNumbers[1]) {
+        answer1 = bot.id.toString()
+      }
+      break botLoop
+    }
+  }
+
+  return {
+    bots,
+    outputs,
+    answer1
+  }
 }
 
 const BUTTONS: IButton[] = [
   {
     label: 'Find Bot',
     onClick: inputKey => {
-      const outputs: Map<number, number> = new Map()
-      const bots = getBots(inputKey)
-      let answer1: false | string = false
-
-      const comparisonNumbers = inputKey.indexOf('DEMO') !== -1
-        ? [2, 5]
-        : [17, 61]
-
-      while (!answer1) {
-        botLoop:
-        for (const bot of bots.values()) {
-          if (!bot.numbers || bot.numbers.length !== 2) continue botLoop
-          const low = Math.min(...bot.numbers)
-          const high = Math.max(...bot.numbers)
-          bot.numbers = []
-          if (bot.low) {
-            if (bot.low.to === 'bot') {
-              const toBotLow = bots.get(bot.low.id)
-              if (toBotLow) {
-                if (!toBotLow.numbers) toBotLow.numbers = []
-                toBotLow.numbers.push(low)
-              }
-            }
-            if (bot.low.to === 'output') {
-              outputs.set(bot.low.id, low)
-            }
-          }
-          if (bot.high) {
-            if (bot.high.to === 'bot') {
-              const toBotHigh = bots.get(bot.high.id)
-              if (toBotHigh) {
-                if (!toBotHigh.numbers) toBotHigh.numbers = []
-                toBotHigh.numbers.push(high)
-              }
-            }
-            if (bot.high.to === 'output') {
-              outputs.set(bot.high.id, high)
-            }
-          }
-          if (low === comparisonNumbers[0] && high === comparisonNumbers[1]) {
-            answer1 = bot.id.toString()
-          }
-          break botLoop
-        }
-      }
+      const { answer1 } = runBots(inputKey)
 
       return {
         answer1
+      }
+    }
+  },
+  {
+    label: 'Find Value',
+    onClick: inputKey => {
+      const { outputs } = runBots(inputKey)
+
+      return {
+        answer2: ((outputs.get(0) || 1) * (outputs.get(1) || 1) * (outputs.get(2) || 1)).toString()
       }
     }
   }
@@ -122,7 +148,8 @@ const config: IDayConfig = {
   ),
   answer2Text: (answer) => (
     <span>
-      <code>{answer}</code>
+      The value of outputs <code>0</code>, <code>1</code>, and <code>2</code>{' '}
+      multiplied by each other is <code>{answer}</code>.
     </span>
   ),
   buttons: BUTTONS,
