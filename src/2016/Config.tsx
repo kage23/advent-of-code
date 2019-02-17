27421df7aef0781a4d57d48e037871cecf4272d3
Kyle Sessions <kage23@gmail.com>
2019-02-16 16:48:23 -0800

    Part 1.

--- src/2016/Config.tsx
+++ src/2016/Config.tsx
@@ -11,6 +11,7 @@ import Day4 from './Day4'
 import Day5 from './Day5'
 import Day6 from './Day6'
 import Day7 from './Day7'
+import Day8 from './Day8'
 
 const CONFIG: IYearConfig = {
   year: 2016,
@@ -27,7 +28,8 @@ const CONFIG: IYearConfig = {
     Day4,
     Day5,
     Day6,
-    Day7
+    Day7,
+    Day8
   ]
 }
 
