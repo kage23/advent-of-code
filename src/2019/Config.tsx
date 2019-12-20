5a732b2850c2a9ec8d1280643d58e445d99d5e94
Kyle G Sessions <kage23@gmail.com>
2019-12-19 20:26:41 -0800

    Day 1, 2019.
    Part 1.
    
    Part 2.

--- src/2019/Config.tsx
+++ src/2019/Config.tsx
@@ -4,7 +4,7 @@ import { IYearConfig } from '../Config'
 
 import coverImage from './cover.png'
 
-// import Day1__2019__Config from './Day1'
+import Day1__2019__Config from './Day1'
 // import Day2__2019__Config from './Day2'
 // import Day3__2019__Config from './Day3'
 // import Day4__2019__Config from './Day4'
@@ -42,7 +42,7 @@ const CONFIG_2019: IYearConfig = {
     </span>
   ),
   days: [
-    // Day1__2019__Config,
+    Day1__2019__Config,
     // Day2__2019__Config,
     // Day3__2019__Config,
     // Day4__2019__Config,
