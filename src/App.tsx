cb01e33cb86a1c9a26c98ddfdfbdb8ed494f94fd
Kyle Sessions <kage23@gmail.com>
2020-02-29 20:10:25 -0800

    Name and style update.
    Make it a little bit different...

--- src/App.tsx
+++ src/App.tsx
@@ -149,7 +149,7 @@ class App extends Component<{}, {
       <div>
         <header>
           <h1>
-            <a onClick={this.reset}>Advent of Code</a>
+            <a onClick={this.reset}>December-Holiday-Approaching-Time Coding Challenge</a>
           </h1>
           <nav>
             <span className="wrapper">
@@ -178,7 +178,7 @@ class App extends Component<{}, {
             ---{' '}
             {dayConfig && dayConfig.title.length > 0
               ? `Day ${day < 10 ? `0${day}` : day}: ${dayConfig.title}`
-              : 'Advent of Code'
+              : 'December-Holiday-Approaching-Time Coding Challenge'
             }
             {' '}---
             {' '}
@@ -196,7 +196,7 @@ class App extends Component<{}, {
                 target="_blank"
                 href="https://adventofcode.com"
               >
-                Advent of Code
+                December-Holiday-Approaching-Time Coding Challenge
               </a>
               {' '}challenges! To get started, select a year and day above.{' '}
               Please note that there's no way for you to enter your custom unique{' '}
@@ -210,7 +210,7 @@ class App extends Component<{}, {
           {yearConfig && !dayConfig && (
             <div>
               <p>
-                Welcome to my {year} Advent of Code solution suite! Select a day above to see my solution{' '}
+                Welcome to my {year} December-Holiday-Approaching-Time Coding Challenge solution suite! Select a day above to see my solution{' '}
                 for that day's challenges. Check out my overall year progress:
               </p>
               <img
