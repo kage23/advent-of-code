1c65e003e62284a95b0a12d670cea8b417eca3b7
Kyle Sessions <kage23@gmail.com>
2020-12-13 14:57:50 -0800

    Day 13, parts 1 and 2.

--- src/2020/Config.tsx
+++ src/2020/Config.tsx
@@ -16,7 +16,7 @@ import Day9__2020__Config from './Day9'
 import Day10__2020__Config from './Day10'
 import Day11__2020__Config from './Day11'
 import Day12__2020__Config from './Day12'
-// import Day13__2020__Config from './Day13'
+import Day13__2020__Config from './Day13'
 // import Day14__2020__Config from './Day14'
 // import Day15__2020__Config from './Day15'
 // import Day16__2020__Config from './Day16'
@@ -62,7 +62,7 @@ const CONFIG_2020: IYearConfig = {
     Day10__2020__Config,
     Day11__2020__Config,
     Day12__2020__Config,
-    // Day13__2020__Config,
+    Day13__2020__Config,
     // Day14__2020__Config,
     // Day15__2020__Config,
     // Day16__2020__Config,
