f3e9e00d2ba4cc9742b3b8c9b77303193e4ead0e
Kyle Sessions <ksessions@quantcast.com>
2020-12-21 11:35:32 -0800

    Day 21, parts 1 and 2.

--- src/2020/Config.tsx
+++ src/2020/Config.tsx
@@ -24,7 +24,7 @@ import Day17__2020__Config from './Day17'
 import Day18__2020__Config from './Day18'
 import Day19__2020__Config from './Day19'
 import Day20__2020__Config from './Day20'
-// import Day21__2020__Config from './Day21'
+import Day21__2020__Config from './Day21'
 // import Day22__2020__Config from './Day22'
 // import Day23__2020__Config from './Day23'
 // import Day24__2020__Config from './Day24'
@@ -50,6 +50,9 @@ const CONFIG_2020: IYearConfig = {
       <p>
         Busses and more flights... The whole thing is traveling to the vacation, LOL.
       </p>
+      <p>
+        On a raft, nearly there...
+      </p>
     </Fragment>
   ),
   days: [
@@ -73,7 +76,7 @@ const CONFIG_2020: IYearConfig = {
     Day18__2020__Config,
     Day19__2020__Config,
     Day20__2020__Config,
-    // Day21__2020__Config,
+    Day21__2020__Config,
     // Day22__2020__Config,
     // Day23__2020__Config,
     // Day24__2020__Config,
