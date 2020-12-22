058ee39f5da759b0bd2a455b5cc56cba41d25acd
Kyle Sessions <ksessions@quantcast.com>
2020-12-22 10:22:22 -0800

    Day 22, parts 1 and 2.

--- src/2020/Config.tsx
+++ src/2020/Config.tsx
@@ -25,7 +25,7 @@ import Day18__2020__Config from './Day18'
 import Day19__2020__Config from './Day19'
 import Day20__2020__Config from './Day20'
 import Day21__2020__Config from './Day21'
-// import Day22__2020__Config from './Day22'
+import Day22__2020__Config from './Day22'
 // import Day23__2020__Config from './Day23'
 // import Day24__2020__Config from './Day24'
 // import Day25__2020__Config from './Day25'
@@ -77,7 +77,7 @@ const CONFIG_2020: IYearConfig = {
     Day19__2020__Config,
     Day20__2020__Config,
     Day21__2020__Config,
-    // Day22__2020__Config,
+    Day22__2020__Config,
     // Day23__2020__Config,
     // Day24__2020__Config,
     // Day25__2020__Config
