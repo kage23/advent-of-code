98e07ab4d1f2f3166294804063c5efe1b83f4db4
Kyle Sessions <ksessions@quantcast.com>
2021-11-30 13:46:20 -0800

    2021 initial setup.

--- src/Config.tsx
+++ src/Config.tsx
@@ -6,6 +6,7 @@ import CONFIG_2017 from './2017/Config'
 import CONFIG_2018 from './2018/Config'
 import CONFIG_2019 from './2019/Config'
 import CONFIG_2020 from './2020/Config'
+import CONFIG_2021 from './2021/Config'
 
 export interface IButton {
   label: string
@@ -53,5 +54,6 @@ export const YEARS: IYearConfig[] = [
   CONFIG_2017,
   CONFIG_2018,
   CONFIG_2019,
-  CONFIG_2020
+  CONFIG_2020,
+  CONFIG_2021
 ]
