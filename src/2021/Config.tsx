3469f430bb93853898dd64ea184366b93c151181
Kyle Sessions <kage23@gmail.com>
2021-12-02 22:05:14 -0800

    Day 3.

--- src/2021/Config.tsx
+++ src/2021/Config.tsx
@@ -6,7 +6,7 @@ import coverImage from './cover.png'
 
 import Day1__2021__Config from './Day1'
 import Day2__2021__Config from './Day2'
-// import Day3__2021__Config from './Day3'
+import Day3__2021__Config from './Day3'
 // import Day4__2021__Config from './Day4'
 // import Day5__2021__Config from './Day5'
 // import Day6__2021__Config from './Day6'
@@ -41,7 +41,7 @@ const CONFIG_2021: IYearConfig = {
   days: [
     Day1__2021__Config,
     Day2__2021__Config,
-    // Day3__2021__Config,
+    Day3__2021__Config,
     // Day4__2021__Config,
     // Day5__2021__Config,
     // Day6__2021__Config,
