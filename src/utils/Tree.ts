6601cf52762499815b84ce58e68e6a6a51423749
Kyle G Sessions <kage23@gmail.com>
2022-12-07 11:33:19 -0800

    2022/day 07 (#83)
    * Part 1 in progress.
    
    Right answer on demo, wrong answer on real. I think the problem is that
    I'm not accounting for directories at different nesting levels with the
    same name, ie `/aa`, `/bb/aa`, `/cc/bb/aa`, etc. That's just a guess at
    the bug though. If I start accounting for that and still get it wrong,
    then we'll see...
    
    * Part 1 solved!
    
    * Part 2 complete!
    
    * Clean up and finish up.
    
    Co-authored-by: Kyle Sessions <ksessions@quantcast.com>

--- src/utils/Tree.ts
+++ src/utils/Tree.ts
@@ -1,36 +1,36 @@
-class Tree {
-  head: TreeNode
-  values: Map<unknown, TreeNode>
+class Tree<T> {
+  head: TreeNode<T>
+  values: Map<T, TreeNode<T>>
 
-  constructor(value: unknown) {
+  constructor(value: T) {
     this.head = new TreeNode(value, this)
     this.values = new Map()
     this.values.set(value, this.head)
   }
 }
 
-export class TreeNode {
-  branches: TreeNode[]
-  parent: TreeNode | undefined
-  tree: Tree
-  value: unknown
+export class TreeNode<T> {
+  branches: TreeNode<T>[]
+  parent: TreeNode<T> | undefined
+  tree: Tree<T>
+  value: T
 
-  constructor(value: unknown, tree: Tree, parent?: TreeNode) {
+  constructor(value: T, tree: Tree<T>, parent?: TreeNode<T>) {
     this.branches = []
     this.parent = parent
     this.tree = tree
     this.value = value
   }
 
-  assignChildNode(newChild: TreeNode): void {
+  assignChildNode(newChild: TreeNode<T>): void {
     this.branches.push(newChild)
     this.tree.values.set(newChild.value, newChild)
   }
 
   getDepth = (): number => this.getPathToHead().length - 1
 
-  getPathToHead(): unknown[] {
-    let current: TreeNode = this
+  getPathToHead(): T[] {
+    let current: TreeNode<T> = this
     const path = [current.value]
     while (current.parent !== undefined) {
       current = current.parent
@@ -39,7 +39,7 @@ export class TreeNode {
     return path
   }
 
-  push(value: unknown): number {
+  push(value: T): number {
     if (!this.tree.values.has(value)) {
       const node = new TreeNode(value, this.tree, this)
       this.branches.push(node)
@@ -50,7 +50,7 @@ export class TreeNode {
     }
   }
 
-  reassignParent(newParent: TreeNode): void {
+  reassignParent(newParent: TreeNode<T>): void {
     if (this.tree !== newParent.tree) throw new Error('fuck')
     this.parent = newParent
     newParent.assignChildNode(this)
