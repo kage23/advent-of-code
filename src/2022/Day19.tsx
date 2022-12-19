74d4ef519af5f5cc16ceb0da784bdef02cc7c783
Kyle Sessions <ksessions@quantcast.com>
2022-12-19 14:02:39 -0800

    part 2 is just a lot of efficiency improvements

--- src/2022/Day19.tsx
+++ src/2022/Day19.tsx
@@ -24,65 +24,62 @@ const parseBlueprint = (raw: string): Blueprint => {
     ore: parseInt(raw.split('ore robot costs ')[1]),
     clay: 0,
     obsidian: 0,
-    type: 'ore'
+    type: 'ore',
   }
   const clayRobot: Robot = {
     ore: parseInt(raw.split('clay robot costs ')[1]),
     clay: 0,
     obsidian: 0,
-    type: 'clay'
+    type: 'clay',
   }
   const obsidianRobot: Robot = {
     ore: parseInt(raw.split('obsidian robot costs ')[1]),
     clay: parseInt(raw.split(' ore and ')[1]),
     obsidian: 0,
-    type: 'obsidian'
+    type: 'obsidian',
   }
   const geodeRobot: Robot = {
     ore: parseInt(raw.split('geode robot costs ')[1]),
     clay: 0,
     obsidian: parseInt(raw.split(' ore and ')[2]),
-    type: 'geode'
+    type: 'geode',
   }
   return {
-    id, oreRobot, clayRobot, obsidianRobot, geodeRobot
+    id,
+    oreRobot,
+    clayRobot,
+    obsidianRobot,
+    geodeRobot,
   }
 }
 
-const getMostGeodes = (blueprint: Blueprint): number => {
+const getMostGeodes = (blueprint: Blueprint, totalTime: number): number => {
   const timerLabel = `Getting geodes with blueprint id ${blueprint.id}`
   console.time(timerLabel)
 
   const scoreFunction = (stateToScore: string): number => {
-    const [
-      oreRobots,
-      clayRobots,
-      obsidianRobots,
-      geodeRobots,
-      ore,
-      clay,
-      obsidian,
-      geodes,
-      time
-    ] = stateToScore.split(',').map(n => Number(n))
-    const timeRemaining = 24 - time
-    return (
-      ((geodes + (geodeRobots * timeRemaining)) * 1) +
-      ((obsidian + (obsidianRobots * timeRemaining)) * 0.1) +
-      ((clay + (clayRobots * timeRemaining)) * 0.01) +
-      ((ore + (oreRobots * timeRemaining)) * 0.001)
-    )
+    const [, , , , , , , , time] = stateToScore.split(',').map((n) => Number(n))
+    return time
   }
 
-  const searchQueue = new BinaryHeap<string>(scoreFunction, 'max', '1,0,0,0,0,0,0,0,0')
+  const searchQueue = new BinaryHeap<string>(
+    scoreFunction,
+    'max',
+    '1,0,0,0,0,0,0,0,0'
+  )
 
   let maxGeodes = 0
 
   while (searchQueue.size()) {
-    const state = searchQueue.pop()!.split(',').map(n => Number(n))
+    const state = searchQueue
+      .pop()!
+      .split(',')
+      .map((n) => Number(n))
     const [, , , , , , , geodes, time] = state
-    if (time === 24) maxGeodes = Math.max(maxGeodes, geodes)
-    getNextStates(state, blueprint).forEach(ns => searchQueue.push(ns))
+    if (time >= totalTime) maxGeodes = Math.max(maxGeodes, geodes)
+    getNextStates(state, blueprint, totalTime).forEach((ns) =>
+      searchQueue.push(ns)
+    )
   }
 
   console.timeEnd(timerLabel)
@@ -90,89 +87,127 @@ const getMostGeodes = (blueprint: Blueprint): number => {
   return maxGeodes
 }
 
-const howLongWillItTakeToHarvest = (amountNeeded: number, currentAmount: number, howManyRobots: number): number => {
+const howLongWillItTakeToHarvest = (
+  amountNeeded: number,
+  currentAmount: number,
+  howManyRobots: number
+): number => {
   if (currentAmount >= amountNeeded) return 0
   // How long will it take to gather supplies though?
   const leftoverNeeded = amountNeeded - currentAmount
   return Math.ceil(leftoverNeeded / howManyRobots)
 }
 
-const getNextStates = ([
-  oreRobots,
-  clayRobots,
-  obsidianRobots,
-  geodeRobots,
-  ore,
-  clay,
-  obsidian,
-  geodes,
-  time
-]: number[], {
-  oreRobot: oreRobotCost, clayRobot: clayRobotCost, obsidianRobot: obsidianRobotCost, geodeRobot: geodeRobotCost
-}: Blueprint): string[] => {
-  if (time >= 24) return []
+const getNextStates = (
+  [
+    oreRobots,
+    clayRobots,
+    obsidianRobots,
+    geodeRobots,
+    ore,
+    clay,
+    obsidian,
+    geodes,
+    time,
+  ]: number[],
+  {
+    oreRobot: oreRobotCost,
+    clayRobot: clayRobotCost,
+    obsidianRobot: obsidianRobotCost,
+    geodeRobot: geodeRobotCost,
+  }: Blueprint,
+  totalTime: number
+): string[] => {
+  if (time >= totalTime) return []
   const nextStates: string[] = []
-  const nextRobots = [oreRobotCost, clayRobotCost, obsidianRobotCost, geodeRobotCost]
+  const nextRobots = [
+    oreRobotCost,
+    clayRobotCost,
+    obsidianRobotCost,
+    geodeRobotCost,
+  ]
 
-  const maxOreRobotsNeeded = Math.max(oreRobotCost.ore, clayRobotCost.ore, obsidianRobotCost.ore, geodeRobotCost.ore)
+  const maxOreRobotsNeeded = Math.max(
+    clayRobotCost.ore,
+    obsidianRobotCost.ore,
+    geodeRobotCost.ore
+  )
   const maxClayRobotsNeeded = obsidianRobotCost.clay
   const maxObsidianRobotsNeeded = geodeRobotCost.obsidian
 
-  const timeRemaining = 24 - time
+  const timeRemaining = totalTime - time
 
   // The decision point for the search tree branches is what robot to build next
   // (including how long that takes to gather supplies)
-  nextRobots.forEach(({ ore: oreCost, clay: clayCost, obsidian: obsidianCost, type }) => {
-    if (
-      (ore >= oreCost || oreRobots > 0) &&
-      (clay >= clayCost || clayRobots > 0) &&
-      (obsidian >= obsidianCost || obsidianRobots > 0)
-    ) {
-      // We are able to build this robot (eventually or now). But how long will it take?
-      const timeToOre = howLongWillItTakeToHarvest(oreCost, ore, oreRobots)
-      const timeToClay = howLongWillItTakeToHarvest(clayCost, clay, clayRobots)
-      const timeToObsidian = howLongWillItTakeToHarvest(obsidianCost, obsidian, obsidianRobots)
-      // It takes one minute to build a robot, after we have enough supplies
-      const timeToRobot = Math.max(timeToOre, timeToClay, timeToObsidian) + 1
+  nextRobots.forEach(
+    ({ ore: oreCost, clay: clayCost, obsidian: obsidianCost, type }) => {
       if (
-        // Do we have enough time to do it?
-        (timeToRobot <= timeRemaining) &&
-        // Do we not have enough of that bot already?
-        (
-          (type === 'ore' && (oreRobots < maxOreRobotsNeeded || oreRobots >= timeRemaining)) ||
-          (type === 'clay' && (clayRobots < maxClayRobotsNeeded || clayRobots >= timeRemaining)) ||
-          (type === 'obsidian' && (obsidianRobots < maxObsidianRobotsNeeded || clayRobots >= timeRemaining)) ||
-          // We never have enough geode robots
-          type === 'geode'
-        )
+        (ore >= oreCost || oreRobots > 0) &&
+        (clay >= clayCost || clayRobots > 0) &&
+        (obsidian >= obsidianCost || obsidianRobots > 0)
       ) {
-        nextStates.push([
-          type === 'ore' ? oreRobots + 1 : oreRobots,
-          type === 'clay' ? clayRobots + 1 : clayRobots,
-          type === 'obsidian' ? obsidianRobots + 1 : obsidianRobots,
-          type === 'geode' ? geodeRobots + 1 : geodeRobots,
-          ore + (oreRobots * timeToRobot) - oreCost,
-          clay + (clayRobots * timeToRobot) - clayCost,
-          obsidian + (obsidianRobots * timeToRobot) - obsidianCost,
-          geodes + (geodeRobots * timeToRobot),
-          time + timeToRobot
-        ].join(','))
+        // We are able to build this robot (eventually or now). But how long will it take?
+        const timeToOre = howLongWillItTakeToHarvest(oreCost, ore, oreRobots)
+        const timeToClay = howLongWillItTakeToHarvest(
+          clayCost,
+          clay,
+          clayRobots
+        )
+        const timeToObsidian = howLongWillItTakeToHarvest(
+          obsidianCost,
+          obsidian,
+          obsidianRobots
+        )
+        // It takes one minute to build a robot, after we have enough supplies
+        const timeToRobot = Math.max(timeToOre, timeToClay, timeToObsidian) + 1
+        if (
+          // Do we have enough time to do it?
+          timeToRobot <= timeRemaining &&
+          // Do we not have enough of that bot already?
+          ((type === 'ore' &&
+            ore + oreRobots * timeRemaining <
+              maxOreRobotsNeeded * timeRemaining) ||
+            (type === 'clay' &&
+              clay + clayRobots * timeRemaining <
+                maxClayRobotsNeeded * timeRemaining) ||
+            (type === 'obsidian' &&
+              obsidian + obsidianRobots * timeRemaining <
+                maxObsidianRobotsNeeded * timeRemaining) ||
+            // We never have enough geode robots
+            type === 'geode')
+        ) {
+          nextStates.push(
+            [
+              type === 'ore' ? oreRobots + 1 : oreRobots,
+              type === 'clay' ? clayRobots + 1 : clayRobots,
+              type === 'obsidian' ? obsidianRobots + 1 : obsidianRobots,
+              type === 'geode' ? geodeRobots + 1 : geodeRobots,
+              ore + oreRobots * timeToRobot - oreCost,
+              clay + clayRobots * timeToRobot - clayCost,
+              obsidian + obsidianRobots * timeToRobot - obsidianCost,
+              geodes + geodeRobots * timeToRobot,
+              time + timeToRobot,
+            ].join(',')
+          )
+        }
       }
     }
-  })
+  )
 
   if (timeRemaining > 0 && nextStates.length === 0) {
-    nextStates.push([
-      oreRobots,
-      clayRobots,
-      obsidianRobots,
-      geodeRobots,
-      ore + (oreRobots * timeRemaining),
-      clay + (clayRobots * timeRemaining),
-      obsidian + (obsidianRobots * timeRemaining),
-      geodes + (geodeRobots * timeRemaining),
-      24
-    ].join(','))
+    nextStates.push(
+      [
+        oreRobots,
+        clayRobots,
+        obsidianRobots,
+        geodeRobots,
+        ore + oreRobots * timeRemaining,
+        clay + clayRobots * timeRemaining,
+        obsidian + obsidianRobots * timeRemaining,
+        geodes + geodeRobots * timeRemaining,
+        totalTime,
+      ].join(',')
+    )
   }
 
   return nextStates
@@ -188,36 +223,68 @@ const BUTTONS: IButton[] = [
 
       const blueprints = INPUT[inputKey].split('\n').map(parseBlueprint)
 
-      const geodeCounts: Map<number, number> = new Map(blueprints.map(blueprint => (
-        [blueprint.id, getMostGeodes(blueprint)]
-      )))
+      const geodeCounts: Map<number, number> = new Map(
+        blueprints.map((blueprint) => [
+          blueprint.id,
+          getMostGeodes(blueprint, 24),
+        ])
+      )
 
-      const totalQualityLevels = Array.from(geodeCounts.entries()).reduce((currentSum, [id, maxGeodes]) => (
-        currentSum + (id * maxGeodes)
-      ), 0)
+      const totalQualityLevels = Array.from(geodeCounts.entries()).reduce(
+        (currentSum, [id, maxGeodes]) => currentSum + id * maxGeodes,
+        0
+      )
 
       console.timeEnd(timerLabel)
 
       // 1804 is too low
 
       return {
-        answer1: totalQualityLevels.toString()
+        answer1: totalQualityLevels.toString(),
       }
-    }
-  }
+    },
+  },
+  {
+    label: 'Get Geodes in More Time',
+    onClick: (inputKey: string) => {
+      const timerLabel = `Get geodes in more time for ${inputKey}`
+      console.time(timerLabel)
+
+      const blueprints = INPUT[inputKey]
+        .split('\n')
+        .slice(0, 3)
+        .map(parseBlueprint)
+
+      const geodeCounts: Map<number, number> = new Map(
+        blueprints.map((blueprint) => [
+          blueprint.id,
+          getMostGeodes(blueprint, 32),
+        ])
+      )
+
+      console.log(geodeCounts)
+
+      console.timeEnd(timerLabel)
+
+      return {
+        answer2: Array.from(geodeCounts.values())
+          .reduce((a, b) => a * b, 1)
+          .toString(),
+      }
+    },
+  },
 ]
 
 const config: IDayConfig = {
   answer1Text: (answer) => (
     <span>
-      The total quality level of all blueprints is{' '}
-      <code>{answer}</code>.
+      The total quality level of all blueprints is <code>{answer}</code>.
     </span>
   ),
   answer2Text: (answer) => (
     <span>
-      The outer surface area of the exposed droplets is{' '}
-      <code>{answer}</code>.
+      The product of the geode count for the first three blueprints in 32
+      minutes is <code>{answer}</code>.
     </span>
   ),
   buttons: BUTTONS,
