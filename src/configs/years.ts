c62678784451f3a7565f407a4c71cd5ed4d15a90
Kyle Sessions <kage23@gmail.com>
2022-12-21 18:34:39 -0800

    2016 setup.

--- src/configs/years.ts
+++ src/configs/years.ts
@@ -1,6 +1,10 @@
 import { YearConfig } from '../routes/Year'
 import year2015 from './2015/config'
+import year2016 from './2016/config'
 
-const years: Map<number, YearConfig> = new Map([[2015, year2015]])
+const years: Map<number, YearConfig> = new Map([
+  [2015, year2015],
+  [2016, year2016],
+])
 
 export default years
