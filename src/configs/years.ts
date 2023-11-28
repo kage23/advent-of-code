39b7a112698f31276524057f00d61a85cd5c29c2
Kyle Sessions <ksessions@quantcast.com>
2023-11-28 14:40:21 -0800

    2017 setup.

--- src/configs/years.ts
+++ src/configs/years.ts
@@ -1,10 +1,12 @@
 import { YearConfig } from '../routes/Year'
 import year2015 from './2015/config'
 import year2016 from './2016/config'
+import year2017 from './2017/config'
 
 const years: Map<number, YearConfig> = new Map([
   [2015, year2015],
   [2016, year2016],
+  [2017, year2017],
 ])
 
 export default years
