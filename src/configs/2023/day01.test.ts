af710dcb0805a2707bb62820a6fa277878ad0be7
Kyle Sessions <kage23@gmail.com>
2023-11-30 21:33:06 -0800

    2023 Day 01.

--- src/configs/2023/day01.test.ts
+++ src/configs/2023/day01.test.ts
@@ -1,14 +1,17 @@
-import {} from './day01'
+import inputs from '../../inputs/2023/day01'
+import { findCalibrationValues, findCalibrationValuesSpelled } from './day01'
 
 describe('2023 Day 01', () => {
   describe('Part 1', () => {
-    it('should find blah blah', () => {
-      // expect(findConnectedToZero('DEMO')!.answer1).toEqual(6)
+    it('should find the calibration sum', () => {
+      expect(findCalibrationValues(inputs.get('DEMO_1')!).answer1).toEqual(142)
     })
   })
   describe('Part 2', () => {
-    it('should count blah blah', () => {
-      // expect(countGroups('DEMO')!.answer2).toEqual(2)
+    it('should find the calibration sum with spelled out numbers', () => {
+      expect(
+        findCalibrationValuesSpelled(inputs.get('DEMO_2')!).answer2
+      ).toEqual(281)
     })
   })
 })
