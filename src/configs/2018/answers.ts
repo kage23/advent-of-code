568403cab59214d13a51df4b474df6f7cd45d7fc
Kyle Sessions <kage23@gmail.com>
2023-12-04 12:45:20 -0800

    2018 Setup.

---
+++ src/configs/2018/answers.ts
@@ -0,0 +1,8 @@
+export {} // import { solveCaptcha1, solveCaptcha2 } from './day01'
+
+describe('2018 Answers', () => {
+  it('Day 01', () => {
+    // console.log(`Day 01, Part 1: ${solveCaptcha1('REAL').answer1}`)
+    // console.log(`Day 01, Part 2: ${solveCaptcha2('REAL').answer2}`)
+  })
+})
--- src/configs/years.ts
+++ src/configs/years.ts
@@ -2,11 +2,13 @@ import { YearConfig } from '../routes/Year'
 import year2015 from './2015/config'
 import year2016 from './2016/config'
 import year2017 from './2017/config'
+import year2018 from './2018/config'
 
 const years: Map<number, YearConfig> = new Map([
   [2015, year2015],
   [2016, year2016],
   [2017, year2017],
+  [2018, year2018],
 ])
 
 export default years
