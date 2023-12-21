fdf3846597621a48793866fdeb7ae33baec84ab8
Kyle Sessions <ksessions@quantcast.com>
2023-12-21 12:14:30 -0800

    Fix routing.

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,35 @@ import Home from './routes/Home'
 import Root from './routes/Root'
 import Year, { loader as yearLoader } from './routes/Year'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+        {
+          path: '',
+          element: <Home />,
+        },
+        {
+          path: ':year/',
+          element: <Year />,
+          id: 'year',
+          loader: yearLoader,
+        },
+        {
+          path: ':year/day/:day/',
+          element: <Day />,
+          id: 'day',
+          loader: dayLoader,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-      {
-        path: ':year/',
-        element: <Year />,
-        id: 'year',
-        loader: yearLoader,
-      },
-      {
-        path: ':year/day/:day/',
-        element: <Day />,
-        id: 'day',
-        loader: dayLoader,
-      },
-    ],
-  },
-])
+    basename: '/advent-of-code'
+  }
+)
 
 const App = () => <RouterProvider router={router} />
 
