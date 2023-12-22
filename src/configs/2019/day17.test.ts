b18049cd9a4883375d5c284998e3eb4302bd4362
Kyle Sessions <ksessions@quantcast.com>
2023-12-22 11:34:00 -0800

    2019 Day 17.

---
+++ src/configs/2019/day17.test.ts
@@ -0,0 +1,16 @@
+// import inputs from '../../inputs/2019/day17'
+import {} from './day17'
+
+// No demo input so no tests
+describe('2019 Day 17', () => {
+  describe('Part 1', () => {
+    xit('should ', () => {
+      //
+    })
+  })
+  describe('Part 2', () => {
+    xit('should ', () => {
+      //
+    })
+  })
+})
