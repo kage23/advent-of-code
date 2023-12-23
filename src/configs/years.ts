ee31d262d53d3d681da3855a31813172a53fa0ab
Kyle Sessions <kage23@gmail.com>
2023-12-22 18:55:15 -0800

    2020 setup.

--- src/configs/years.ts
+++ src/configs/years.ts
@@ -4,6 +4,7 @@ import year2016 from './2016/config'
 import year2017 from './2017/config'
 import year2018 from './2018/config'
 import year2019 from './2019/config'
+import year2020 from './2020/config'
 
 const years: Map<number, YearConfig> = new Map([
   [2015, year2015],
@@ -11,6 +12,7 @@ const years: Map<number, YearConfig> = new Map([
   [2017, year2017],
   [2018, year2018],
   [2019, year2019],
+  [2020, year2020],
 ])
 
 export default years
