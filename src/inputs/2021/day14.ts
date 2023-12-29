0a531cda18fc16f138140b705d86bb62d8c6daf5
Kyle Sessions <ksessions@quantcast.com>
2023-12-29 15:26:25 -0800

    2021 Day 14.

---
+++ src/inputs/2021/day14.ts
@@ -0,0 +1,25 @@
+const inputs: Map<string, string> = new Map([
+  [
+    'DEMO',
+    `NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`,
+  ],
+])
+
+export default inputs
