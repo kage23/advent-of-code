adee794043dc6d57d774d12b903005551ddc696a
Kyle Sessions <ksessions@quantcast.com>
2024-01-08 16:14:50 -0800

    2022 Days 2 and 3.

--- src/configs/2022/config.ts
+++ src/configs/2022/config.ts
@@ -1,8 +1,8 @@
 import { YearConfig } from '../../routes/Year'
 import cover from './cover.png'
 import day01 from './day01'
-// import day02 from './day02'
-// import day03 from './day03'
+import day02 from './day02'
+import day03 from './day03'
 // import day04 from './day04'
 // import day05 from './day05'
 // import day06 from './day06'
@@ -30,8 +30,8 @@ const year2022: YearConfig = {
   cover,
   days: new Map([
     [1, day01],
-    // [2, day02],
-    // [3, day03],
+    [2, day02],
+    [3, day03],
     // [4, day04],
     // [5, day05],
     // [6, day06],
