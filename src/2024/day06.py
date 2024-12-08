d57c1b0ae5bb87d169d7ce6a2df9317bf4f94d1d
Kyle Sessions <kage23@gmail.com>
2024-12-07 21:39:46 -0800

    part 2

--- src/2024/day06.py
+++ src/2024/day06.py
@@ -1,7 +1,8 @@
 def main():
-  with open("src/2024/inputs/day06-sample.txt") as file:
-    print(f"the guard will visit {part_1(file.readlines())} distinct positions")
-    print(f"there are {part_2(file.readlines())} possible locations for the new obstruction")
+  with open("src/2024/inputs/day06.txt") as file:
+    field = file.readlines()
+    print(f"the guard will visit {part_1(field)} distinct positions")
+    print(f"there are {part_2(field)} possible locations for the new obstruction")
 
 
 # 0: ^
@@ -17,20 +18,27 @@ def part_1(field):
   return len(guard_positions)
 
 
-# part 2 doesn't work and needs to be fixed!!!
 def part_2(field):
   count = 0
-  obstructions, guard = parse_map(field)
-  for row in range(len(field)):
-    for col in range(len(field[row])):
-      new_guard = [*guard]
-      new_obstructions = [*obstructions, (row, col)]
-      guard_positions = set()
-      while new_guard not in guard_positions and 0 <= new_guard[0] < len(field) and 0 <= new_guard[1] < len(field[0]):
-        guard_positions.add(new_guard)
+  obstructions, starting_guard = parse_map(field)
+  guard = [*starting_guard]
+  guard_positions = set()
+  while 0 <= guard[0] < len(field) and 0 <= guard[1] < len(field[0]):
+    guard_positions.add((guard[0], guard[1]))
+    guard = get_next_guard(obstructions, guard)
+  for position in guard_positions:
+    if position[0] != starting_guard[0] or position[1] != starting_guard[1]:
+      new_obstructions = [*obstructions, (position[0], position[1])]
+      new_guard = [*starting_guard]
+      new_guard_positions = set()
+      while True:
+        new_guard_positions.add((new_guard[0], new_guard[1], new_guard[2]))
         new_guard = get_next_guard(new_obstructions, new_guard)
-      if new_guard in guard_positions:
-        count += 1
+        if (new_guard[0], new_guard[1], new_guard[2]) in new_guard_positions:
+          count += 1
+          break
+        if new_guard[0] < 0 or new_guard[0] >= len(field) or new_guard[1] < 0 or new_guard[1] >= len(field[0]):
+          break
   return count
 
 
