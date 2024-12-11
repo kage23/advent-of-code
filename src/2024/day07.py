c26a1f2b78ef2843b7bf29ed4ecf85e249deae31
Kyle Sessions <ksessions@tvscientific.com>
2024-12-11 15:10:57 -0800

    part 2

--- src/2024/day07.py
+++ src/2024/day07.py
@@ -5,22 +5,25 @@ def main():
   with open("src/2024/inputs/day07.txt") as file:
     lines = file.readlines()
     print(f"the total calibration result is {part_1(lines)}")
-    # print(f"there are {part_2(field)} possible locations for the new obstruction")
+    print(f"the actual calibration result is {part_2(lines)}")
 
 
 def part_1(lines):
   result = 0
   for line in lines:
-    result += evaluate_line(line)
+    result += evaluate_line(line.strip())
   return result
 
 
 def part_2(lines):
-  ...
+  result = 0
+  for line in lines:
+    result += evaluate_line(line.strip(), True)
+  return result
 
 
-def evaluate_line(line):
-  target, starting_nodes = parse_line(line)
+def evaluate_line(line, part2=False):
+  target, starting_nodes = parse_line(line, part2)
   search_queue = [*starting_nodes]
   while len(search_queue) > 0:
     node = search_queue.pop()
@@ -28,7 +31,7 @@ def evaluate_line(line):
       if node["values"][0] == target:
         return target
     else:
-      value = (node["values"][0] + node["values"][1]) if node["next_op"] == "+" else (node["values"][0] * node["values"][1])
+      value = get_next_value(node)
       new_values = [value, *node["values"][2:]]
       search_queue.append({
         "values": new_values,
@@ -38,14 +41,19 @@ def evaluate_line(line):
         "values": new_values,
         "next_op": "*",
       })
+      if part2:
+        search_queue.append({
+        "values": new_values,
+        "next_op": "||",
+      })
   return 0
 
 
-def parse_line(line):
+def parse_line(line, part2):
   t, v = line.split(": ")
   target = int(t)
   values = list(map(int, v.split(" ")))
-  return target, [
+  nodes = [
     {
       "values": values,
       "next_op": "+",
@@ -55,6 +63,23 @@ def parse_line(line):
       "next_op": "*",
     }
   ]
+  if part2:
+    nodes.append({
+      "values": values,
+      "next_op": "||",
+    })
+  return target, nodes
+
+
+def get_next_value(node):
+  next_value = 0
+  if node["next_op"] == "+":
+    next_value = node["values"][0] + node["values"][1]
+  elif node["next_op"] == "*":
+    next_value = node["values"][0] * node["values"][1]
+  elif node["next_op"] == "||":
+    next_value = int(f"{node["values"][0]}{node["values"][1]}")
+  return next_value
 
 
 if __name__ == "__main__":
