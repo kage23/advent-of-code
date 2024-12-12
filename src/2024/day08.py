9042047d717126e9f4e24318eb2ace2e6ee89669
Kyle Sessions <kage23@gmail.com>
2024-12-11 17:05:40 -0800

    part 2

--- src/2024/day08.py
+++ src/2024/day08.py
@@ -5,7 +5,7 @@ def main():
   with open("src/2024/inputs/day08.txt") as file:
     lines = file.readlines()
     print(f"there are {part_1(lines)} unique antinode locations")
-    # print(f"the actual calibration result is {part_2(lines)}")
+    print(f"there are actually {part_2(lines)} unique antinode locations")
 
 
 def part_1(lines):
@@ -22,7 +22,15 @@ def part_1(lines):
 
 
 def part_2(lines):
-  ...
+  antennae = parse_input(lines)
+  antinodes = set()
+  for _, locations in antennae.items():
+    for x in range(len(locations) - 1):
+      for y in range(x+1, len(locations)):
+        local_antinodes = get_antinodes_part_2(locations[x], locations[y], len(lines), len(lines[0].strip()))
+        for antinode in local_antinodes:
+          antinodes.add(antinode)
+  return len(antinodes)
 
 
 def parse_input(lines):
@@ -43,6 +51,30 @@ def get_antinodes(location_1, location_2):
     (location_1[0] - row_diff, location_1[1] - col_diff),
     (location_2[0] + row_diff, location_2[1] + col_diff),
   ]
+
+
+def get_antinodes_part_2(location_1, location_2, height, width):
+  row_diff = location_2[0] - location_1[0]
+  col_diff = location_2[1] - location_1[1]
+  antinodes = [location_1, location_2]
+  next_row, next_col = location_1[0], location_1[1]
+  while True:
+    next_row -= row_diff
+    next_col -= col_diff
+    if 0 <= next_row < height and 0 <= next_col < width:
+      antinodes.append((next_row, next_col))
+    else:
+      break
+  next_row, next_col = location_2[0], location_2[1]
+  while True:
+    next_row += row_diff
+    next_col += col_diff
+    if 0 <= next_row < height and 0 <= next_col < width:
+      antinodes.append((next_row, next_col))
+    else:
+      break
+  return antinodes
+
 
 
 if __name__ == "__main__":
