33ebad0d1c5accd469284ecbf7c3e2be6044786b
Kyle Sessions <ksessions@tvscientific.com>
2024-12-18 12:41:14 -0800

    part 2 complete

--- src/2024/day17.py
+++ src/2024/day17.py
@@ -10,27 +10,31 @@ def main():
     c = parse("Register C: {}", lines[2])
     p = parse("Program: {}", lines[4])
     program = list(map(int, p.fixed[0].split(",")))
-    print(f"the final output is {part_1(int(a.fixed[0]), int(b.fixed[0]), int(c.fixed[0]), program)}")
-    print(f"the value in A that will cause the program to output itself is {part_2(int(b.fixed[0]), int(c.fixed[0]), program)}")
+    print(f"the final output is {part_1(int(a.fixed[0]), program)}")
+    print(f"the lowest value in A that will cause the program to output itself is {part_2(program)}")
 
 
-def part_1(a, b, c, program):
-  elf_computer = ElfComputer(a, b, c)
+def part_1(a, program):
+  elf_computer = ElfComputer(a)
   return elf_computer.run_program(program)
 
 
-# This is a brute-force approach which will probably take way too long
-def part_2(b, c, program):
-  program_str = ",".join(map(str, program))
-  a = 0
-  while True:
-    if a % 100000 == 0:
-      print(f"testing {a}")
-    elf_computer = ElfComputer(a, b, c)
-    if elf_computer.run_program(program) == program_str:
-      break
-    a += 1
-  return a
+def part_2(program):
+  possible_starts = ["0o"]
+  for i in range(len(program)):
+    next_possible_starts = []
+    target = program[0-i-1]
+    for start in possible_starts:
+      for j in range(8):
+        possible_oct = f"{start}{j}"
+        a = int(possible_oct[2:], 8)
+        elf_computer = ElfComputer(a)
+        elf_computer.run_program(program)
+        if (elf_computer.output[0] == target):
+          if i == len(program) - 1:
+            return a
+          next_possible_starts.append(possible_oct)
+    possible_starts = [*next_possible_starts]
 
 
 if __name__ == "__main__":
