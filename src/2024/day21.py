5b9377203cc02ad49549ca271f50b802043d7a03
Kyle Sessions <ksessions@tvscientific.com>
2024-12-24 17:18:57 -0800

    what the fuck is even happening

--- src/2024/day21.py
+++ src/2024/day21.py
@@ -1,146 +1,247 @@
-from math import inf
+from functools import cache
 
 
-NUMPAD_BUTTON_LOCATIONS = {
-  "7": (0, 0),
-  "8": (0, 1),
-  "9": (0, 2),
-  "4": (1, 0),
-  "5": (1, 1),
-  "6": (1, 2),
-  "1": (2, 0),
-  "2": (2, 1),
-  "3": (2, 2),
-  "BLANK": (3, 0),
-  "0": (3, 1),
-  "A": (3, 2),
+NUMPAD_DIRECTIONS = {
+  ("A", "A"): "A",
+  ("A", "0"): "<A",
+  ("A", "1"): "^<<A",
+  ("A", "2"): "<^A",
+  ("A", "3"): "^A",
+  ("A", "4"): "^^<<A",
+  ("A", "5"): "<^^A",
+  ("A", "6"): "^^A",
+  ("A", "7"): "^^^<<A",
+  ("A", "8"): "<^^^A",
+  ("A", "9"): "^^^A",
+  ("0", "A"): ">A",
+  ("0", "0"): "A",
+  ("0", "1"): "^<A",
+  ("0", "2"): "^A",
+  ("0", "3"): "^>A",
+  ("0", "4"): "^^<A",
+  ("0", "5"): "^^A",
+  ("0", "6"): "^^>A",
+  ("0", "7"): "^^^<A",
+  ("0", "8"): "^^^A",
+  ("0", "9"): "^^^>A",
+  ("1", "A"): ">>vA",
+  ("1", "0"): ">vA",
+  ("1", "1"): "A",
+  ("1", "2"): ">A",
+  ("1", "3"): ">>A",
+  ("1", "4"): "^A",
+  ("1", "5"): "^>A",
+  ("1", "6"): "^>>A",
+  ("1", "7"): "^^A",
+  ("1", "8"): "^^>A",
+  ("1", "9"): "^^>>A",
+  ("2", "A"): "v>A",
+  ("2", "0"): "vA",
+  ("2", "1"): "<A",
+  ("2", "2"): "A",
+  ("2", "3"): ">A",
+  ("2", "4"): "<^A",
+  ("2", "5"): "^A",
+  ("2", "6"): "^>",
+  ("2", "7"): "<^^A",
+  ("2", "8"): "^^A",
+  ("2", "9"): "^^>A",
+  ("3", "A"): "vA",
+  ("3", "0"): "<vA",
+  ("3", "1"): "<<A",
+  ("3", "2"): "<A",
+  ("3", "3"): "A",
+  ("3", "4"): "<<^A",
+  ("3", "5"): "<^A",
+  ("3", "6"): "^A",
+  ("3", "7"): "<<^^A",
+  ("3", "8"): "<^^A",
+  ("3", "9"): "^^A",
+  ("4", "A"): ">>vvA",
+  ("4", "0"): ">vvA",
+  ("4", "1"): "vA",
+  ("4", "2"): "v>A",
+  ("4", "3"): "v>>A",
+  ("4", "4"): "A",
+  ("4", "5"): ">A",
+  ("4", "6"): ">>A",
+  ("4", "7"): "^A",
+  ("4", "8"): "^>A",
+  ("4", "9"): "^>>A",
+  ("5", "A"): "vv>A",
+  ("5", "0"): "vvA",
+  ("5", "1"): "<vA",
+  ("5", "2"): "vA",
+  ("5", "3"): "v>A",
+  ("5", "4"): "<A",
+  ("5", "5"): "A",
+  ("5", "6"): ">A",
+  ("5", "7"): "<^A",
+  ("5", "8"): "^A",
+  ("5", "9"): "^>A",
+  ("6", "A"): "vvA",
+  ("6", "0"): "<vvA",
+  ("6", "1"): "<<vA",
+  ("6", "2"): "<vA",
+  ("6", "3"): "vA",
+  ("6", "4"): "<<A",
+  ("6", "5"): "<A",
+  ("6", "6"): "A",
+  ("6", "7"): "<<^A",
+  ("6", "8"): "<^A",
+  ("6", "9"): "^A",
+  ("7", "A"): ">>vvvA",
+  ("7", "0"): ">vvvA",
+  ("7", "1"): "vvA",
+  ("7", "2"): "vv>A",
+  ("7", "3"): "vv>>A",
+  ("7", "4"): "vA",
+  ("7", "5"): "v>A",
+  ("7", "6"): "v>>A",
+  ("7", "7"): "A",
+  ("7", "8"): ">A",
+  ("7", "9"): ">>A",
+  ("8", "A"): "vvv>A",
+  ("8", "0"): "vvvA",
+  ("8", "1"): "<vvA",
+  ("8", "2"): "vvA",
+  ("8", "3"): "vv>A",
+  ("8", "4"): "<vA",
+  ("8", "5"): "vA",
+  ("8", "6"): "v>A",
+  ("8", "7"): "<A",
+  ("8", "8"): "A",
+  ("8", "9"): ">A",
+  ("9", "A"): "vvvA",
+  ("9", "0"): "<vvvA",
+  ("9", "1"): "<<vvA",
+  ("9", "2"): "<vvA",
+  ("9", "3"): "vvA",
+  ("9", "4"): "<<vA",
+  ("9", "5"): "<vA",
+  ("9", "6"): "vA",
+  ("9", "7"): "<<A",
+  ("9", "8"): "<A",
+  ("9", "9"): "A",
 }
 
 
-ARROW_BUTTON_LOCATIONS = {
-  "BLANK": (0, 0),
-  "^": (0, 1),
-  "A": (0, 2),
-  "<": (1, 0),
-  "v": (1, 1),
-  ">": (1, 2),
+ARROW_DIRECTIONS = {
+  ("A", "A"): "A",
+  ("A", "^"): "<A",
+  ("A", "<"): "v<<A",
+  ("A", "v"): "<vA",
+  ("A", ">"): "vA",
+  ("^", "A"): ">A",
+  ("^", "^"): "A",
+  ("^", "<"): "v<A",
+  ("^", "v"): "vA",
+  ("^", ">"): "v>A",
+  ("<", "A"): ">>^A",
+  ("<", "^"): ">^A",
+  ("<", "<"): "A",
+  ("<", "v"): ">A",
+  ("<", ">"): ">>A",
+  ("v", "A"): "^>A",
+  ("v", "^"): "^A",
+  ("v", "<"): "<A",
+  ("v", "v"): "A",
+  ("v", ">"): ">A",
+  (">", "A"): "^A",
+  (">", "^"): "<^A",
+  (">", "<"): "<<A",
+  (">", "v"): "<A",
+  (">", ">"): "A",
 }
 
 
 def main():
-  numpad_directions = generate_all_button_directions(NUMPAD_BUTTON_LOCATIONS, 3, 4)
-  arrow_directions = generate_all_button_directions(ARROW_BUTTON_LOCATIONS, 3, 2)
   with open("src/2024/inputs/day21-sample.txt") as file:
     codes = file.read().split("\n")
-    print(f"the sum of the complexities of all codes in the list is {part_1(codes, numpad_directions, arrow_directions)}")
-    print(f"the sum of the complexities of all codes in the list with more robots is {part_2(codes, numpad_directions, arrow_directions)}")
+    print(f"the sum of the complexities of all codes in the list is {part_1(codes)}")
+    print(f"the sum of the complexities of all codes in the list with more robots is {part_2(codes)}")
 
 
-def part_1(codes, numpad_directions, arrow_directions):
+def part_1(codes):
   final_sum = 0
   for code in codes:
-    final_paths = []
-    first_robot_paths = get_robot_instructions(code, numpad_directions)
-    for path in first_robot_paths:
-      second_robot_paths = get_robot_instructions("".join(path), arrow_directions)
-      for path2 in second_robot_paths:
-        third_robot_paths = get_robot_instructions("".join(path2), arrow_directions)
-        for path3 in third_robot_paths:
-          final_paths.append(path3)
-    final_path_length = min(list(map(lambda p: len(p), final_paths)))
-    complexity = final_path_length * int(code[:-1])
+    first_robot_path = get_robot_instructions(code, "numpad")
+    second_robot_path = get_robot_instructions(first_robot_path, "arrow")
+    third_robot_path = get_robot_instructions(second_robot_path, "arrow")
+    complexity = len(third_robot_path) * int(code[:-1])
     final_sum += complexity
   return final_sum
 
 
-def part_2(codes, numpad_directions, arrow_directions):
+def part_2(codes):
   final_sum = 0
   for code in codes:
-    targets = [code]
-    for i in range(3):
-      for t in targets:
-        paths = get_robot_instructions(t, numpad_directions if i == 0 else arrow_directions)
-        targets = paths
-    final_path_length = min(list(map(lambda p: len(p), targets)))
-    complexity = final_path_length * int(code[:-1])
+    robot_path = get_robot_instructions(code, "numpad")
+    for _ in range(25):
+      robot_path = get_robot_instructions(robot_path, "arrow")
+    complexity = len(robot_path) * int(code[:-1])
     final_sum += complexity
   return final_sum
 
 
-def get_robot_instructions(code, button_directions):
-  paths = []
-  position = "A"
-  for x in code:
-    if x == position:
-      paths = list(map(lambda p: [*p, "A"], paths))
-    else:
-      possible_paths = button_directions[(position, x)]
-      if len(paths) == 0:
-        paths = list(map(lambda pp: [*pp, "A"], possible_paths))
-      else:
-        new_paths = []
-        for p in paths:
-          for pp in possible_paths:
-            new_paths.append([*p, *pp, "A"])
-        paths = new_paths
-      position = x
-  return fewest_move_sequences(paths)
+# def build_key_sequence(keys, index, prev_key, curr_path, result):
+#   # print(f"build_key_sequence for {keys}")
+#   if index == len(keys):
+#     # print(f"final return: {[*result, curr_path]}")
+#     return [*result, curr_path]
+#   result_to_flatten = list(map(
+#     lambda path: build_key_sequence(keys, index + 1, keys[index], curr_path + path + 'A', result),
+#     ARROW_DIRECTIONS[(prev_key, keys[index])]
+#   ))
+#   # print(f"in progress return: {[x for xs in result_to_flatten for x in xs]}")
+#   return [x for xs in result_to_flatten for x in xs]
 
 
-def generate_all_button_directions(button_locations, width, height):
-  button_directions = {}
-  blank = button_locations["BLANK"]
-  for i, (button_1, position_1) in enumerate(button_locations.items()):
-    for j, (button_2, position_2) in enumerate(button_locations.items()):
-      if j != i and button_1 != "BLANK" and button_2 != "BLANK":
-        directions = get_button_to_button_directions(position_1, position_2, blank, width, height)
-        button_directions[(button_1, button_2)] = directions
-  return button_directions
+# def get_shortest_sequence(keys, depth, cache):
+#   if depth == 0:
+#     if keys == "A":
+#       # print(f"get_shortest_sequence {keys} depth zero return: {0}")
+#       return 0
+#     # print(f"get_shortest_sequence {keys} depth zero return: {len(keys)}")
+#     return len(keys)
+#   if (keys, depth) in cache:
+#     # print(f"get_shortest_sequence {keys} cache return: {cache[(keys, depth)]}")
+#     return cache[(keys, depth)]
+#   sub_keys = keys.strip().split("A")
+#   total = 0
+#   for sub_key in sub_keys:
+#     sub_key += "A"
+#     sequences = build_key_sequence(sub_key, 0, "A", "", [], arrow_directions)
+#     minimum = min(map(
+#       lambda s: get_shortest_sequence(s, depth - 1, cache, arrow_directions),
+#       sequences
+#     ))
+#     total += minimum
+#   cache[(keys, depth)] = total
+#   # print(f"get_shortest_sequence {keys} normal return: {total}")
+#   return total
 
 
-def get_button_to_button_directions(position_1, position_2, blank, width, height):
-  directions = {
-    "^": (-1, 0),
-    "v": (1, 0),
-    "<": (0, -1),
-    ">": (0, 1)
-  }
-  paths = []
-  shortest_path_length = inf
-  search_queue = [{
-    "position": position_1,
-    "path": [],
-    "visited": [position_1],
-  }]
-  while search_queue:
-    node = search_queue.pop()
-    if len(node["path"]) <= shortest_path_length:
-      if node["position"] == position_2:
-        paths.append(node["path"])
-        shortest_path_length = min(shortest_path_length, len(node["path"]))
-      else:
-        if len(node["path"]) + 1 <= shortest_path_length:
-          for arrow, dir in directions.items():
-            new_position = (node["position"][0] + dir[0], node["position"][1] + dir[1])
-            if new_position != blank and 0 <= new_position[0] < height and 0 <= new_position[1] < width and new_position not in node["visited"]:
-              search_queue.append({
-                "position": new_position,
-                "path": [*node["path"], arrow],
-                "visited": [*node["visited"], new_position]
-              })
-  return fewest_move_sequences(list(filter(lambda p: len(p) <= shortest_path_length, paths)))
+@cache
+def get_robot_instructions(code, button_type):
+  button_directions = NUMPAD_DIRECTIONS if button_type == "numpad" else ARROW_DIRECTIONS
+  path = ""
+  for i, to_key in enumerate(code):
+    from_key = code[i-1] if i != 0 else "A"
+    path += button_directions[(from_key, to_key)]
+  return path
 
 
-def fewest_move_sequences(paths):
-  moves = {}
-  for path in paths:
-    path_moves = 0
-    for i in range(1, len(path)):
-      if path[i] != path[i - 1]:
-        path_moves += 1
-    moves[",".join(path)] = path_moves
-  fewest_moves = min(moves.values())
-  return list(filter(lambda p: moves[",".join(p)] == fewest_moves, paths))
+# def fewest_move_sequences(paths):
+#   moves = {}
+#   for path in paths:
+#     path_moves = 0
+#     for i in range(1, len(path)):
+#       if path[i] != path[i - 1]:
+#         path_moves += 1
+#     moves[",".join(path)] = path_moves
+#   fewest_moves = min(moves.values())
+#   return list(filter(lambda p: moves[",".join(p)] == fewest_moves, paths))
 
 
 if __name__ == "__main__":
