a86b76b354b711c47159cc72507059f90eb84150
Kyle Sessions <kage23@gmail.com>
2025-12-01 19:30:52 -0800

    finished day 1 part 2

--- src/2025/day01.py
+++ src/2025/day01.py
@@ -1,5 +1,5 @@
 def main():
-    with open("./inputs/day01-sample.txt") as file:
+    with open("./inputs/day01.txt") as file:
         lines = file.readlines()
     part_1(lines)
     part_2(lines)
@@ -20,48 +20,35 @@ def part_1(lines):
     print(f"part 1 zero count: {zero_count}")
 
 
-# not 5656
 def part_2(lines):
     dial = 50
     zero_count = 0
     for line in lines:
         dir = line[0]
         dist = int(line[1:])
-        # print("dial", dial)
-        # print(dir, dist)
-        while dist >= 100:
-            if dial != 0:
-                zero_count += 1
-            dist -= 100
-        new_dial = dial + dist if dir == 'R' else dial - dist
-        if new_dial < 0:
-            if dial != 0:
-                zero_count += 1
-            new_dial += 100
-        elif new_dial > 99:
-            zero_count += 1
-            new_dial -= 100
-        elif new_dial == 0:
-            zero_count += 1
-        dial = new_dial
-        # print("new_dial", dial)
-        # print("zero_count", zero_count)
-        # print()
-        # print()
-
-
-
-        # if dir == 'L':
-        #     dial -= dist
-        # elif dir == 'R':
-        #     dial += dist
-        # if dial < 0 or dial > 99:
-        #     zero_count += 1
-        #     dial = dial % 100
-        # if dial == 0:
-        #     zero_count += 1
+        dial, zero_count = turn_dial(dial, dir, dist, zero_count)
     print(f"part 2 zero count: {zero_count}")
 
+
+def turn_dial(dial, dir, dist, zero_count):
+    if dir == 'R':
+        dial = dial + dist
+        while dial >= 100:
+            dial -= 100
+            zero_count += 1
+        return dial, zero_count
+    elif dir == 'L':
+        new_dial = dial - dist
+        if new_dial < 0:
+            while new_dial < 0:
+                new_dial += 100
+                zero_count += 1
+        if new_dial == 0:
+            zero_count += 1
+        if dial == 0: zero_count -= 1  # this is dumb but if we started at 0, we double counted it
+        return new_dial, zero_count
+
+    return dial, zero_count
 
 
 if __name__ == "__main__":
