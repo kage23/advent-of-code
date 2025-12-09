b0f9e0e112996fdc240c2e88aefbfd650f580345
Kyle Sessions <ksessions@tvscientific.com>
2025-12-09 11:41:09 -0800

    2025 day 9 part 2 still broken but broken differently

--- src/2025/day09.py
+++ src/2025/day09.py
@@ -1,12 +1,12 @@
 def main():
-    with open("./inputs/day09.txt") as file:
-        lines = file.readlines()
-    part_1(lines)
-    part_2(lines)
+    with open("./inputs/day09-sample.txt") as file:
+        coords = file.readlines()
+    part_1(coords)
+    part_2(coords)
 
 
-def part_1(lines):
-    tiles = list(map(lambda line: list(map(int, line.split(','))), lines))
+def part_1(coords):
+    tiles = list(map(lambda line: list(map(int, line.split(','))), coords))
     areas = []
     for i, tile in enumerate(tiles):
         for j in range(i + 1, len(tiles)):
@@ -18,18 +18,20 @@ def part_1(lines):
 
 # not 4738108384
 # not 4582310446 - too high
-def part_2(lines):
-    tiles = list(map(lambda line: list(map(int, line.split(','))), lines))
+def part_2(coords):
+    tiles = list(map(lambda line: list(map(int, line.split(','))), coords))
     areas = []
+    shape_lines = get_lines(tiles)
     for i, tile1 in enumerate(tiles):
         for j in range(i + 1, len(tiles)):
             tile2 = tiles[j]
             tile3 = [tile2[0], tile1[1]]
             tile4 = [tile1[0], tile2[1]]
+            rectangle_lines = get_lines([tile1, tile3, tile2, tile4])
             if (
                 is_contained(tile3, tiles) and
                 is_contained(tile4, tiles) and
-                all(not_in(tile, tile1, tile2, tile3, tile4) for tile in tiles)
+                no_crossing_lines(rectangle_lines, shape_lines)
             ):
                 areas.append(get_area(tile1, tile2))
     areas.sort()
@@ -71,6 +73,38 @@ def not_in(tile, tile1, tile2, tile3, tile4):
         min(tile1[0], tile2[0]) <= tile[0] <= max(tile1[0], tile2[0]) and
         min(tile1[1], tile2[1]) <= tile[1] <= max(tile1[1], tile2[1])
     )
+
+
+def get_lines(coords):
+    lines = []
+    for i, coord in enumerate(coords):
+        prev_coord = coords[i - 1]
+        line = { 'x': list({prev_coord[0], coord[0]}), 'y': list({prev_coord[1], coord[1]}) }
+        if len(line['x']) > 1 or len(line['y']) > 1:
+            str_line = str(line)
+            if str_line not in lines:
+                lines.append(str_line)
+    return list(map(eval, lines))
+
+
+def no_crossing_lines(rectangle, shape):
+    for l1 in rectangle:
+        for l2 in shape:
+            if l1 != l2 and they_cross(l1, l2):
+                return False
+    return True
+
+
+def they_cross(l1, l2):
+    l1_is_vertical = len(l1['x']) == 1
+    l2_is_vertical = len(l2['x']) == 1
+    if l1_is_vertical == l2_is_vertical:
+        return False
+    if l1_is_vertical:
+        return min(l2['x']) < l1['x'][0] + 0.1 < max(l2['x'])
+    else:
+        return min(l2['y']) < l1['y'][0] + 0.1 < max(l2['y'])
+
 
 if __name__ == "__main__":
   main()
